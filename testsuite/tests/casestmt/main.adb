bd0a28aa3605a93d7c2c9591b12c781cb23a5660
Pascal Obry <obry@adacore.com>
2016-03-01 10:41:50 +0100

    Add some simple tests.
    Part of P113-046.
    
    Change-Id: I8251f1a3c08d34a9b0e6ac21fbedb39398a1c51b

--- testsuite/tests/casestmt/main.adb
+++ testsuite/tests/casestmt/main.adb
@@ -23,10 +23,10 @@
 ------------------------------------------------------------------------------
 
 with Ada.Text_IO;
+
 with GPR2.Project.View;
 with GPR2.Project.Tree;
 with GPR2.Project.Attribute.Set;
-with GPR2.Project.Variable.Set;
 with GPR2.Context;
 
 procedure Main is
@@ -37,24 +37,12 @@ procedure Main is
 
    procedure Display (Prj : Project.View.Object; Full : Boolean := True);
 
-   procedure Changed_Callback (Prj : Project.View.Object);
-
-   ----------------------
-   -- Changed_Callback --
-   ----------------------
-
-   procedure Changed_Callback (Prj : Project.View.Object) is
-   begin
-      Text_IO.Put_Line (">>> Changed_Callback for " & Value (Prj.Path_Name));
-   end Changed_Callback;
-
    -------------
    -- Display --
    -------------
 
    procedure Display (Prj : Project.View.Object; Full : Boolean := True) is
       use GPR2.Project.Attribute.Set.Set;
-      use GPR2.Project.Variable.Set.Set;
    begin
       Text_IO.Put (Prj.Name & " ");
       Text_IO.Set_Col (10);
@@ -63,7 +51,7 @@ procedure Main is
       if Full then
          if Prj.Has_Attributes then
             for A in Prj.Attributes.Iterate loop
-               Text_IO.Put ("A:   " & String (Key (A)));
+               Text_IO.Put ("   " & String (Key (A)));
                Text_IO.Put (" ->");
 
                for V of Element (A).Values loop
@@ -72,68 +60,21 @@ procedure Main is
                Text_IO.New_Line;
             end loop;
          end if;
-
-         if Prj.Has_Variables then
-            for V in Prj.Variables.Iterate loop
-               Text_IO.Put ("V:   " & String (Key (V)));
-               Text_IO.Put (" -> ");
-               Text_IO.Put (String (Element (V).Value));
-               Text_IO.New_Line;
-            end loop;
-         end if;
-         Text_IO.New_Line;
       end if;
    end Display;
 
-   Prj1, Prj2 : Project.Tree.Object;
-   Ctx        : Context.Object;
+   Prj : Project.Tree.Object;
+   Ctx : Context.Object;
 
 begin
-   Prj1 := Project.Tree.Load (Create ("demo.gpr"));
-   Prj2 := Project.Tree.Load (Create ("demo.gpr"));
+   Prj := Project.Tree.Load (Create ("demo.gpr"));
 
-   Ctx := Prj1.Context;
    Ctx.Include ("OS", "Linux");
-   Prj1.Set_Context (Ctx, Changed_Callback'Access);
-
-   Ctx := Prj2.Context;
-   Ctx.Include ("OS", "Windows");
-   Prj2.Set_Context (Ctx, Changed_Callback'Access);
-
-   Display (Prj1.Root_Project);
-   Display (Prj2.Root_Project);
+   Prj.Set_Context (Ctx);
+   Display (Prj.Root_Project);
 
    Ctx.Clear;
-   Ctx.Include ("OS", "Linux-2");
-   Prj2.Set_Context (Ctx, Changed_Callback'Access);
-   Display (Prj2.Root_Project);
-
-   --  Iterator
-
-   Text_IO.Put_Line ("**************** Iterator Prj1");
-
-   for C in Project.Tree.Iterate (Prj1, Kind => I_Project + I_Imported) loop
-      Display (Project.Tree.Element (C), Full => False);
-      if Project.Tree.Is_Root (C) then
-         Text_IO.Put_Line ("   is root");
-      end if;
-   end loop;
-
-   Text_IO.Put_Line ("**************** Iterator Prj2");
-
-   for C in Project.Tree.Iterate (Prj2) loop
-      Display (Project.Tree.Element (C), Full => False);
-   end loop;
-
-   Text_IO.Put_Line ("**************** Iterator Prj3");
-
-   for C in Project.Tree.Iterate (Prj2, Filter => F_Library) loop
-      Display (Project.Tree.Element (C), Full => False);
-   end loop;
-
-   Text_IO.Put_Line ("**************** Iterator Prj4");
-
-   for P of Prj2 loop
-      Display (P, Full => False);
-   end loop;
+   Ctx.Include ("OS", "Windows");
+   Prj.Set_Context (Ctx);
+   Display (Prj.Root_Project);
 end Main;
---
+++ testsuite/tests/packages/main.adb
@@ -0,0 +1,92 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           GPR2 PROJECT MANAGER                           --
+--                                                                          --
+--            Copyright (C) 2016, Free Software Foundation, Inc.            --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Text_IO;
+
+with GPR2.Project.View;
+with GPR2.Project.Tree;
+with GPR2.Project.Attribute.Set;
+with GPR2.Context;
+
+procedure Main is
+
+   use Ada;
+   use GPR2;
+   use GPR2.Project;
+
+   procedure Display (Prj : Project.View.Object);
+
+   procedure Display (Att : Project.Attribute.Object);
+
+   -------------
+   -- Display --
+   -------------
+
+   procedure Display (Att : Project.Attribute.Object) is
+   begin
+      Text_IO.Put ("   " & Att.Name);
+
+      if Att.Has_Language then
+         Text_IO.Put (" (" & Att.Language & ")");
+      end if;
+
+      Text_IO.Put (" ->");
+
+      for V of Att.Values loop
+         Text_IO.Put (" " & V);
+      end loop;
+      Text_IO.New_Line;
+   end Display;
+
+   procedure Display (Prj : Project.View.Object) is
+      use GPR2.Project.Attribute.Set.Set;
+   begin
+      Text_IO.Put (Prj.Name & " ");
+      Text_IO.Set_Col (10);
+      Text_IO.Put_Line (Prj.Kind'Img);
+
+      if Prj.Has_Attributes then
+         for A of Prj.Attributes loop
+            Display (A);
+         end loop;
+      end if;
+
+      if Prj.Has_Packages then
+         for Pck of Prj.Packages loop
+            Text_IO.Put_Line (" " & String (Pck.Name));
+
+            for A of Pck.Attributes loop
+               Display (A);
+            end loop;
+         end loop;
+
+      end if;
+   end Display;
+
+   Prj : Project.Tree.Object;
+
+begin
+   Prj := Project.Tree.Load (Create ("demo.gpr"));
+   Display (Prj.Root_Project);
+end Main;
