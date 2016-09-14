c218b9cba4b0206b777285b8a0222dbebdfcc618
Pascal Obry <obry@adacore.com>
2016-09-14 08:53:54 +0200

    Add API to handle imports.
    The imported projects will be handled with this API to be able to
    record the sloc of the import and later others status like if it
    is a limited import or not.
    
    Continued work for P113-046.
    
    Change-Id: Ife0506ea8222d8d1ed3079bb9b8f0836fad3e0c9

---
+++ src/gpr2-project-import.adb
@@ -0,0 +1,47 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           GPR2 PROJECT MANAGER                           --
+--                                                                          --
+--            Copyright (C) 2016, Free Software Foundation, Inc.            --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+package body GPR2.Project.Import is
+
+   ------------
+   -- Create --
+   ------------
+
+   function Create
+     (Path_Name : Path_Name_Type;
+      Sloc      : Source_Reference.Object) return Object is
+   begin
+      return Object'(Sloc with Path_Name);
+   end Create;
+
+   ---------------
+   -- Path_Name --
+   ---------------
+
+   function Path_Name (Self : Object) return Path_Name_Type is
+   begin
+      return Self.Path_Name;
+   end Path_Name;
+
+end GPR2.Project.Import;
---
+++ src/gpr2-project-import.ads
@@ -0,0 +1,56 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           GPR2 PROJECT MANAGER                           --
+--                                                                          --
+--            Copyright (C) 2016, Free Software Foundation, Inc.            --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with GPR2.Source_Reference;
+
+package GPR2.Project.Import is
+
+   use type GPR2.Source_Reference.Object;
+
+   type Object is new Source_Reference.Object with private;
+
+   subtype Import_Type is Object;
+
+   Undefined : constant Object;
+
+   function Create
+     (Path_Name : Path_Name_Type;
+      Sloc      : Source_Reference.Object) return Object
+     with Pre => Sloc /= Source_Reference.Undefined;
+
+   function Path_Name (Self : Object) return Path_Name_Type
+     with Pre => Self /= Undefined;
+   --  Full pathname of the corresponding project file
+
+private
+
+   type Object is new Source_Reference.Object with record
+      Path_Name : Path_Name_Type;
+   end record;
+
+   Undefined : constant Object :=
+                 (Source_Reference.Undefined
+                  with Path_Name => No_Path_Name);
+
+end GPR2.Project.Import;
