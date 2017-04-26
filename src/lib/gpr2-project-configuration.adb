5c1da8fad800df19b6cd1b3f23403d313c31b74a
Pascal Obry <obry@adacore.com>
2017-04-26 10:06:22 +0200

    Initial version of configuration API.
    This initial version is using GPRconfig external tool. It is not the
    final version but a simple implementation to provide support for
    configuration data.
    
    Continued work for OB10-014.
    
    Change-Id: I4b60d3f143a8afc0b6129770b8efcb82930e53ae

---
+++ src/lib/gpr2-project-configuration.adb
@@ -0,0 +1,188 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           GPR2 PROJECT MANAGER                           --
+--                                                                          --
+--            Copyright (C) 2017, Free Software Foundation, Inc.            --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Strings.Fixed;
+
+with GNAT.OS_Lib;
+
+with GPR.Tempdir;
+with GPR.Util;
+
+with GPR2.Message;
+with GPR2.Parser.Project;
+with GPR2.Project.Definition;
+
+package body GPR2.Project.Configuration is
+
+   ------------------------
+   -- Corresponding_View --
+   ------------------------
+
+   function Corresponding_View (Self : Object) return Project.View.Object is
+   begin
+      return Self.Conf;
+   end Corresponding_View;
+
+   ------------
+   -- Create --
+   ------------
+
+   function Create
+     (Language : Name_Type;
+      Version  : Optional_Name_Type := No_Name;
+      Runtime  : Optional_Name_Type := No_Name;
+      Path     : Optional_Name_Type := No_Name;
+      Name     : Optional_Name_Type := No_Name) return Description
+   is
+      function "+" (Str : Optional_Name_Type) return Unbounded_String
+        is (To_Unbounded_String (String (Str)));
+   begin
+      return Description'
+        (Language => +Language,
+         Version  => +Version,
+         Runtime  => +Runtime,
+         Path     => +Path,
+         Name     => +Name);
+   end Create;
+
+   ------------------
+   -- Has_Messages --
+   ------------------
+
+   function Has_Messages (Self : Object) return Boolean is
+   begin
+      return not Self.Messages.Is_Empty;
+   end Has_Messages;
+
+   ----------
+   -- Load --
+   ----------
+
+   function Load
+     (Settings : Description_Set;
+      Target   : Name_Type := "all") return Object
+   is
+      --  Note that this is a temporary implementation to bring a solution
+      --  for the configuration support in LibGPR2. The long term and proper
+      --  solution will be implemented later based on the knowledge base API
+      --  which could be reviewed from scratch.
+
+      use Ada;
+      use GNAT;
+
+      function Process_Id return String is
+        (Strings.Fixed.Trim
+           (Integer'Image (OS_Lib.Pid_To_Integer (OS_Lib.Current_Process_Id)),
+            Strings.Both));
+
+      --  Note that Temporary_Directory_Path could be the empty string and in
+      --  this case we just write the temporary file into the current working
+      --  directory.
+
+      Conf_Filename : constant String :=
+                        (if GPR.Tempdir.Temporary_Directory_Path = ""
+                         then ""
+                         else GPR.Util.Ensure_Directory
+                                (GPR.Tempdir.Temporary_Directory_Path))
+                        & Process_Id & "-gpr2_tmp_conf.cgpr";
+
+      GPRconfig : constant OS_Lib.String_Access :=
+                    OS_Lib.Locate_Exec_On_Path ("gprconfig");
+      Args      : OS_Lib.Argument_List (1 .. Settings'Length + 5);
+      Success   : Boolean := False;
+
+      Result    : Object;
+   begin
+      --  Build parameters
+
+      Args (1) := GPRconfig;
+      Args (2) := new String'("-o");
+      Args (3) := new String'(Conf_Filename);
+      Args (4) := new String'("--batch");
+      Args (5) := new String'("--target=" & String (Target));
+
+      for K in Settings'Range loop
+         Args (6 + K - Settings'First) :=
+           new String'("--config="
+                       & To_String (Settings (K).Language)
+                       & "," & To_String (Settings (K).Version)
+                       & "," & To_String (Settings (K).Runtime)
+                       & "," & To_String (Settings (K).Path)
+                       & "," & To_String (Settings (K).Name));
+      end loop;
+
+      --  Execute external GPRconfig tool
+
+      OS_Lib.Spawn (GPRconfig.all, Args, Success);
+
+      --  Free arguments
+
+      for K in Args'Range loop
+         OS_Lib.Free (Args (K));
+      end loop;
+
+      --  Load the configuration object generated if execution was succeful
+
+      Result.Conf := View.Undefined;
+
+      if Success then
+         declare
+            Project : constant Parser.Project.Object :=
+                        Parser.Project.Load
+                          (Create (Name_Type (Conf_Filename)),
+                           Result.Messages);
+            Data    : GPR2.Project.Definition.Data (Has_Context => False);
+         begin
+            --  Continue only if there is no parsing error on the configuration
+            --  project.
+
+            if Result.Messages.Is_Empty then
+               Data.Trees.Project := Project;
+               Data.Context_View := View.Undefined;
+               Data.Status := Definition.Root;
+
+               Result.Conf := Definition.Register (Data);
+            end if;
+         end;
+
+      else
+         Result.Messages.Append
+           (Message.Create
+              (Message.Error,
+               "cannot create configuration file, fail to execute gprconfig"));
+      end if;
+
+      return Result;
+   end Load;
+
+   ------------------
+   -- Log_Messages --
+   ------------------
+
+   function Log_Messages (Self : Object) return Log.Object is
+   begin
+      return Self.Messages;
+   end Log_Messages;
+
+end GPR2.Project.Configuration;
---
+++ src/lib/gpr2-project-configuration.ads
@@ -0,0 +1,85 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           GPR2 PROJECT MANAGER                           --
+--                                                                          --
+--            Copyright (C) 2017, Free Software Foundation, Inc.            --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  Handle the configuration object for a project tree
+
+with GPR2.Log;
+with GPR2.Project.View;
+
+package GPR2.Project.Configuration is
+
+   use type Containers.Count_Type;
+
+   type Object is tagged private;
+
+   function Has_Messages (Self : Object) return Boolean;
+   --  Returns whether some messages are present for this configuration
+
+   function Log_Messages (Self : Object) return Log.Object
+     with Post => not Self.Has_Messages or else Log_Messages'Result.Count > 0;
+   --  Returns the Logs, this contains information, warning and error messages
+   --  found while handling the project.
+
+   type Description is private;
+   --  A configuration description, for a language, a version, specific
+   --  runtime, etc.
+
+   type Description_Set is array (Positive range <>) of Description
+     with Dynamic_Predicate => Description_Set'Length > 0;
+   --  A set of description
+
+   function Create
+     (Language : Name_Type;
+      Version  : Optional_Name_Type := No_Name;
+      Runtime  : Optional_Name_Type := No_Name;
+      Path     : Optional_Name_Type := No_Name;
+      Name     : Optional_Name_Type := No_Name) return Description;
+   --  Returns a description for a configuration. This description object is
+   --  used to retreive the available and corresponding compilers. That is,
+   --  the compilers for the given language, the runtime if specificed, etc.
+
+   function Load
+     (Settings : Description_Set;
+      Target   : Name_Type := "all") return Object;
+   --  Create a configuration based on the settings requested
+
+   function Corresponding_View (Self : Object) return Project.View.Object;
+   --  Get project for the given configuration object
+
+private
+
+   type Description is record
+      Language : Unbounded_String;
+      Version  : Unbounded_String;
+      Runtime  : Unbounded_String;
+      Path     : Unbounded_String;
+      Name     : Unbounded_String;
+   end record;
+
+   type Object is tagged record
+      Messages : Log.Object;
+      Conf     : Project.View.Object;
+   end record;
+
+end GPR2.Project.Configuration;
