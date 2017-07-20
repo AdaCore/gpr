a465432145bc594221ee4e219f588e6db419d7d8
Erwan Le Guillou <leguillou@adacore.com>
2017-07-20 14:45:40 +0200

    Add support for --build option needed by tests in GAIA.
    This is done by setting CROSS_SUPPORT to True.
    
    Change-Id: Iea064621db7f40fa613c2472a8e75c3485155e8f

--- testsuite/testsuite_support/__init__.py
+++ testsuite/testsuite_support/__init__.py
@@ -8,6 +8,7 @@ from testsuite_support.python_script import PythonScriptDriver
 
 class Testsuite(BaseTestsuite):
     TEST_SUBDIR = 'tests'
+    CROSS_SUPPORT = True
     DRIVERS = {
         'build_and_run': BuildAndRunDriver,
         'python_script': PythonScriptDriver,
