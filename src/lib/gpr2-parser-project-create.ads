887cfd1c5c85dfde9fd68304237fdd8f9fd85d0c
Pascal Obry <obry@adacore.com>
2017-07-31 11:00:19 +0200

    Add routine to create a built-in project.
    This will be used to create the runtime project for example.
    
    Part of Q614-013.
    
    Change-Id: If1da88ef06079223b91eaeca5685d2a827dc1998

---
+++ src/lib/gpr2-parser-project-create.ads
@@ -0,0 +1,30 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           GPR2 PROJECT MANAGER                           --
+--                                                                          --
+--            Copyright (C) 2017, Free Software Foundation, Inc.            --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+function GPR2.Parser.Project.Create
+  (Name      : Name_Type;
+   File      : Path_Name_Type;
+   Qualifier : Project_Kind) return Object;
+--  Simple constructor for a project object. This is designed to be used by
+--  built-in project like the runtime one.
