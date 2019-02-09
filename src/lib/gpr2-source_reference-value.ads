5d22e5c7f0ad1833c65f07049958f5efac46f9b9
Pascal Obry <obry@adacore.com>
2019-02-09 19:21:32 +0100

    New unit Source_Reference.Value to record value reference in code.
    Change-Id: If35bdbc3669f9b27d00a9adf3d7e46ddf8cb15f9

---
+++ src/lib/gpr2-source_reference-value.ads
@@ -0,0 +1,31 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           GPR2 PROJECT MANAGER                           --
+--                                                                          --
+--            Copyright (C) 2019, Free Software Foundation, Inc.            --
+--                                                                          --
+-- This library is free software;  you can redistribute it and/or modify it --
+-- under terms of the  GNU General Public License  as published by the Free --
+-- Software  Foundation;  either version 3,  or (at your  option) any later --
+-- version. This library is distributed in the hope that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE.                            --
+--                                                                          --
+-- As a special exception under Section 7 of GPL version 3, you are granted --
+-- additional permissions described in the GCC Runtime Library Exception,   --
+-- version 3.1, as published by the Free Software Foundation.               --
+--                                                                          --
+-- You should have received a copy of the GNU General Public License and    --
+-- a copy of the GCC Runtime Library Exception along with this program;     --
+-- see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see    --
+-- <http://www.gnu.org/licenses/>.                                          --
+--                                                                          --
+------------------------------------------------------------------------------
+
+--  This package represents an entity source reference with an associated
+--  message. This is mostly to report warnings/errors while parsing sources.
+
+with GPR2.Source_Reference.Text_Value;
+
+package GPR2.Source_Reference.Value is
+  new GPR2.Source_Reference.Text_Value (Value_Type);
