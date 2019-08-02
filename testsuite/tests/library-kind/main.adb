deb44a56e5e9d9482c09274d57f75a08c6a56a88
Dmitriy Anisimkov <anisimko@adacore.com>
2019-08-02 20:12:13 +0600

    Fix test output after previous commit
    "emit warning on attributes check failure"
    
    Change-Id: I14364573b51666cda5000aa5e2b711470fcd9a91

--- testsuite/tests/library-kind/main.adb
+++ testsuite/tests/library-kind/main.adb
@@ -21,6 +21,7 @@ with Ada.Text_IO;
 with Ada.Strings.Fixed;
 
 with GPR2.Context;
+with GPR2.Log;
 with GPR2.Message;
 with GPR2.Project.View;
 with GPR2.Project.Tree;
@@ -36,6 +37,9 @@ procedure Main is
 
    procedure Display (Prj : Project.View.Object);
 
+   Prj : Project.Tree.Object;
+   Ctx : Context.Object;
+
    -------------
    -- Display --
    -------------
@@ -48,11 +52,35 @@ procedure Main is
       Text_IO.Put_Line (Image (Prj.Kind));
    end Display;
 
-   Prj : Project.Tree.Object;
-   Ctx : Context.Object;
+   --------------------
+   -- Print_Messages --
+   --------------------
+
+   procedure Print_Messages is
+   begin
+      if Prj.Log_Messages.Has_Element (Information => False) then
+         Text_IO.Put_Line ("Messages found:");
+
+         for M in Prj.Log_Messages.Iterate (Information => False) loop
+            declare
+               Mes : constant String := GPR2.Log.Element (M).Format;
+               L   : constant Natural :=
+                       Strings.Fixed.Index (Mes, "/demo");
+            begin
+               if L /= 0 then
+                  Text_IO.Put_Line (Mes (L .. Mes'Last));
+               else
+                  Text_IO.Put_Line (Mes);
+               end if;
+            end;
+         end loop;
+      end if;
+   end Print_Messages;
 
 begin
    Project.Tree.Load (Prj, Create ("demo.gpr"), Ctx);
+
+   Print_Messages;
 
    --  Iterator
 
@@ -62,27 +90,9 @@ begin
 
    Project.Tree.Load (Prj, Create ("demo2.gpr"), Ctx);
 
-   --  Iterator
+   Print_Messages;
 
 exception
    when GPR2.Project_Error =>
-      if Prj.Has_Messages then
-         Text_IO.Put_Line ("Messages found:");
-
-         for M of Prj.Log_Messages.all loop
-            if M.Level = Message.Error then
-               declare
-                  Mes : constant String := M.Format;
-                  L   : constant Natural :=
-                          Strings.Fixed.Index (Mes, "/demo");
-               begin
-                  if L /= 0 then
-                     Text_IO.Put_Line (Mes (L .. Mes'Last));
-                  else
-                     Text_IO.Put_Line (Mes);
-                  end if;
-               end;
-            end if;
-         end loop;
-      end if;
+      Print_Messages;
 end Main;
