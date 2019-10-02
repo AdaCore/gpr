a672c888b8bfcfc8920b85a710750533800dee7f
Pascal Obry <obry@adacore.com>
2019-10-02 20:57:37 +0200

    GPRinstall needs .gpr in project name.
    Change-Id: I20b5a7e6a8f3b39277e379251e329aca1020ee8d

--- testsuite/tests/gprinstall-manifest/test.py
+++ testsuite/tests/gprinstall-manifest/test.py
@@ -3,7 +3,7 @@ import os, subprocess
 output=subprocess.check_output('gprbuild -p -q simple.gpr', shell=True)
 
 output=subprocess.check_output('gpr2install -p --no-manifest --prefix='
-                               + os.getcwd() + '/inst simple', shell=True)
+                               + os.getcwd() + '/inst simple.gpr', shell=True)
 
 if os.path.exists('inst/share/gpr/manifests/simple'):
     print("NOK")
