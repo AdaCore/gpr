772981055d85c107a788fd3e6a7b14039f8d34d5
Philippe Gil <gil@adacore.com>
2019-11-07 14:56:29 +0100

    Fix SB05-019-separator-at-end-of-project-path on Linux.
    Change-Id: I19c4ed179aa7787cabed1cba5a5234e27e96e6d0

--- testsuite/tests/SB05-019-separator-at-end-of-project-path/test.py
+++ testsuite/tests/SB05-019-separator-at-end-of-project-path/test.py
@@ -1,6 +1,16 @@
-import os, subprocess
+import os
+from gnatpython.ex import Run
 
+# set environment with project paths terminated by path separator ':' or ';'
 os.environ['GPR_PROJECT_PATH'] =  os.getenv('GPR_PROJECT_PATH', '') + os.pathsep
 os.environ['ADA_PROJECT_PATH'] =  os.getenv('ADA_PROJECT_PATH', '') + os.pathsep
 
-subprocess.check_output('gpr2clean -Pp')
+# check that gpr2-projects package is able to handle such environment
+# without raising an exception
+p = Run(['gpr2clean', '-Pp'])
+if p.status != 0:
+    print ('gpr2clean returned ' + str (p.status))
+    print (p.err)
+    print (p.out)
+else:
+    print ('OK')
