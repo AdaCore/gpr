c911e05677a503433576e4ca605175189da0c059
Pierre-Marie de Rodat <derodat@adacore.com>
2020-01-30 05:58:01 -0500

    Remove Quex
    Langkit no longer uses Quex for a year, so remove all obsolete mentions
    of Quex in GPR2.
    
    Change-Id: I66fe87f398b3d88a7fc150cbca64a130c12176ad
    TN: R913-030

--- langkit/quex/quex/engine/state_machine/algebra/cut_end.py
+++
@@ -1,9 +0,0 @@
-import quex.engine.state_machine.algorithm.beautifier as beautifier
-import quex.engine.state_machine.algebra.reverse      as reverse
-import quex.engine.state_machine.algebra.cut_begin    as cut_begin
-
-def do(SM_A, SM_B):
-    Ar        = beautifier.do(reverse.do(SM_A))
-    Br        = beautifier.do(reverse.do(SM_B))
-    cut_Ar_Br = cut_begin.do(Ar, Br)
-    return reverse.do(cut_Ar_Br)
