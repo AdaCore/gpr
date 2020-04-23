d4661c32f57f48c4ef294876ef3902c5939bfb33
Philippe Gil <gil@adacore.com>
2020-04-23 15:56:45 +0200

    fix for configuration-file-error-handling test
    fix os.getcwd() casing issue on windows
    
    part of T422-006
    
    Change-Id: If58046a2e8b00e660b631cca2a3ce62de340da20

--- testsuite/testsuite_support/utils.py
+++ testsuite/testsuite_support/utils.py
@@ -3,6 +3,8 @@ Helper for Python scripts in testcases.
 """
 
 import os
+import re
+from e3.env import Env
 
 
 def filter(output, actual, expected):
@@ -14,4 +16,9 @@ def filter_cwd(output):
     """
     Remove all occurrences of the current working directory from in output.
     """
-    return output.replace(os.getcwd(), '')
+    if Env().host.platform.endswith('windows'):
+        #  On windows, path are case insensitive
+        return re.compile(re.escape(os.getcwd()),
+                          re.IGNORECASE).sub('', output)
+    else:
+        return output.replace(os.getcwd(), '')
