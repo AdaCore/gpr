ba5cf1563694c3f35e62bf9e39a4cdb29aae9396
Dmitriy Anisimkov <anisimko@adacore.com>
2020-06-10 22:48:12 +0600

    Fix for linux and minor optimization
    T421-013
    
    Change-Id: I8efc373eafb362f3cc6cb1f52eb7bd390aeb1f74

--- testsuite/tests/gnat_tool_prefix/test.py
+++ testsuite/tests/gnat_tool_prefix/test.py
@@ -6,25 +6,20 @@ if Env().host.platform.endswith('windows'):
     cp('config-windows.cgpr', 'config.cgpr')
     cp('prj_driver-windows.gpr', 'prj_driver.gpr')
 
-p = Run(['gprbuild', '-p', '-q', 'prj.gpr'])
+Run(['gprbuild', '-p', '-q', 'prj.gpr'], output=None)
 
 # test native without compiler packages
-p = Run(['main', '-P', 'prj.gpr'])
-print(p.out)
+Run(['./main', '-P', 'prj.gpr'], output=None)
 
 # test cross without compiler packages & no compiler installed
-p = Run(['main', '-P', 'prj_arm_eabi.gpr'])
-print(p.out)
+Run(['./main', '-P', 'prj_arm_eabi.gpr'], output=None)
 
 # test using config's compiler package
-p = Run(['main', '--config', 'config.cgpr', '-P', 'prj.gpr'])
-print(p.out)
+Run(['./main', '--config', 'config.cgpr', '-P', 'prj.gpr'], output=None)
 
 # test using project's compiler package
 # under windows check also casing & executable extension support
-p = Run(['main', '-P', 'prj_driver.gpr'])
-print(p.out)
+Run(['./main', '-P', 'prj_driver.gpr'], output=None)
 
 # test different compiler defined in project & config
-p = Run(['main', '--config', 'config.cgpr', '-P', 'prj_driver.gpr'])
-print(p.out)
+Run(['./main', '--config', 'config.cgpr', '-P', 'prj_driver.gpr'], output=None)
