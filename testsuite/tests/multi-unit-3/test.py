21c40de7912049d602b78f16498d76784a4916b8
Philippe Gil <gil@adacore.com>
2021-01-26 16:33:54 +0100

    try to normalize multi-unit-3 outputs.
    part of U114-025
    
    Change-Id: If2a90842b5214b7db2ddee014ded9b0537a94778

--- testsuite/tests/multi-unit-3/test.py
+++ testsuite/tests/multi-unit-3/test.py
@@ -6,7 +6,7 @@ from e3.os.process import Run
 os.mkdir('obj')
 
 # first run without .ali (no compilation)
-r = subprocess.getoutput(['gpr2ls', '-d', 'multi.gpr'])
+r = subprocess.getoutput('gpr2ls -d -P multi.gpr')
 
 print('No build')
 print(r)
@@ -14,7 +14,7 @@ print(r)
 Run(['gprbuild', '-p', '-q', 'multi.gpr'])
 
 # second run with .ali (after compilation)
-r = subprocess.getoutput(['gpr2ls', '-d', 'multi.gpr'])
+r = subprocess.getoutput('gpr2ls -d -P multi.gpr')
 
 print('With build')
 print(r)
