d09ef4c58b2b8941f181cc988243cb75b465bb85
Philippe Gil <gil@adacore.com>
2021-02-09 16:33:39 +0100

    fix obj_dir in aggregated's aggregated project
    --no-precommit-check
    
    TN: U209-054
    
    Change-Id: I9b3e1c6c1f5fd40fbf1f42c79cd33fdd374ffc7c

---
+++ testsuite/tests/agg-obj-dir/main.adb
@@ -0,0 +1,87 @@
+------------------------------------------------------------------------------
+--                                                                          --
+--                           GPR2 PROJECT MANAGER                           --
+--                                                                          --
+--                       Copyright (C) 2021, AdaCore                        --
+--                                                                          --
+-- This is  free  software;  you can redistribute it and/or modify it under --
+-- terms of the  GNU  General Public License as published by the Free Soft- --
+-- ware  Foundation;  either version 3,  or (at your option) any later ver- --
+-- sion.  This software is distributed in the hope  that it will be useful, --
+-- but WITHOUT ANY WARRANTY;  without even the implied warranty of MERCHAN- --
+-- TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public --
+-- License for more details.  You should have received  a copy of the  GNU  --
+-- General Public License distributed with GNAT; see file  COPYING. If not, --
+-- see <http://www.gnu.org/licenses/>.                                      --
+--                                                                          --
+------------------------------------------------------------------------------
+
+with Ada.Text_IO;
+with GPR2.Context;
+with GPR2.Log;
+with GPR2.Path_Name;
+with GPR2.Project.Tree;
+with GPR2.Project.View;
+
+procedure Main is
+   Tree         : GPR2.Project.Tree.Object;
+   Context      : GPR2.Context.Object;
+   use GPR2;
+
+   procedure Print_Messages is
+   begin
+      if Tree.Has_Messages then
+         for C in Tree.Log_Messages.Iterate
+           (False, True, True, True, True)
+         loop
+            Ada.Text_IO.Put_Line (GPR2.Log.Element (C).Format);
+         end loop;
+      end if;
+   end Print_Messages;
+
+   function Get_Aggregated
+     (View : GPR2.Project.View.Object; Name : Simple_Name)
+      return GPR2.Project.View.Object is
+   begin
+      for V of View.Aggregated loop
+         if V.Path_Name.Simple_Name = Name then
+            return V;
+         end if;
+      end loop;
+      return GPR2.Project.View.Undefined;
+   end Get_Aggregated;
+
+   procedure Test (View : GPR2.Project.View.Object) is
+      A     : GPR2.Project.View.Object;
+      B     : GPR2.Project.View.Object;
+   begin
+      Ada.Text_IO.Put_Line ("testing " & String (View.Path_Name.Simple_Name));
+      A := Get_Aggregated (View, "a.gpr");
+      B := Get_Aggregated (View, "b.gpr");
+      Ada.Text_IO.Put_Line ("A.Object_Dir:" & A.Object_Directory.Value);
+      Ada.Text_IO.Put_Line ("B.Object_Dir:" & B.Object_Directory.Value);
+   exception
+      when Project_Error =>
+         Print_Messages;
+   end Test;
+
+   procedure Load (Project_Name : GPR2.Filename_Type) is
+   begin
+      Ada.Text_IO.Put_Line ("loading " & String (Project_Name));
+      Tree.Unload;
+      Tree.Load_Autoconf
+        (Filename => GPR2.Path_Name.Create_File
+           (GPR2.Project.Ensure_Extension (Project_Name),
+            GPR2.Path_Name.No_Resolution),
+         Context  => Context);
+   exception
+      when Project_Error =>
+         Print_Messages;
+   end Load;
+
+begin
+   Load ("files/aggl1.gpr");
+   Test (Tree.Root_Project);
+   Load ("files/aggl.gpr");
+   Test (Get_Aggregated (Tree.Root_Project, "aggl1.gpr"));
+end Main;
