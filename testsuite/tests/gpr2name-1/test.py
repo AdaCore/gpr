6d0426ce19cce0e12ac6d1b2085d4d5444d620c6
Philippe Gil <gil@adacore.com>
2021-04-02 17:36:36 +0200

    change gprname error handling
    let gprname print a message in stderr and return error status
    
    Change-Id: If253df2aa94da99b909054c068a19654f9cd9ccc
    TN:U322-016

--- testsuite/tests/gpr2name-1/test.py
+++ testsuite/tests/gpr2name-1/test.py
@@ -127,3 +127,12 @@ except Exception as E:
     print('*** Error: %s' % str(E))
 finally:
     os.chdir('..')
+
+# invalid gpr handling test
+try:
+    p = BuilderAndRunner().run([GPRNAME, '-Pinvalid-gpr-file/prj', '*.body'])
+    if p.status != 0:
+        print('gprname returned ' + str(p.status))
+        print(p.out)
+except Exception as E:
+    print('*** Error: %s' % str(E))
