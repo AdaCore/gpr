2f4afa49faf950a9cd441bf9971d5f94e29eb731
Philippe Gil <gil@adacore.com>
2021-10-19 13:42:11 +0200

    move F117-038 test from gprbuild to gpr testsuite
    TN: U301-025
    Change-Id: I7a82fc61a34da35aec82377227a1446ae403b0f7

--- testsuite/tests/gpr2name-1/test.py
+++ testsuite/tests/gpr2name-1/test.py
@@ -146,3 +146,22 @@ except Exception as E:
     print('*** Error: %s' % str(E))
 finally:
     os.chdir('..')
+
+# generated project file indentation
+try:
+    os.chdir('pretty-print')
+    for c in 'cdefghijklmno':
+        p = 'subdirectory_' + c
+        if not os.path.exists(p):
+            os.mkdir(p)
+    run([GPRNAME, '-P', 'prj.gpr', '-dsubdirectory_a', '-dsubdirectory_b',
+         '-dsubdirectory_c', '-dsubdirectory_d', '-dsubdirectory_e',
+         '-dsubdirectory_f', '-dsubdirectory_g', '-dsubdirectory_h',
+         '-dsubdirectory_i', '-dsubdirectory_j', '-dsubdirectory_k',
+         '-dsubdirectory_l', '-dsubdirectory_m', '-dsubdirectory_n',
+         '-dsubdirectory_o', '*.ad?'])
+    os.system('cat prj.gpr')
+except Exception as E:
+    print('*** Error: %s' % str(E))
+finally:
+    os.chdir('..')
