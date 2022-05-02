8be945b50e6c5715f195f95fce2b2b759da69e52
Jerome Lambourg <lambourg@adacore.com>
2022-05-02 17:45:17 +0200

    Path_Name: strengthen the implementation and add more tests
    This change now properly handle some specific cases and adds proper
    handling or pre when necessary:
    
    - Base_Name: was incorrectly returning "." for the root directory, while
      this subprogram doesn't make sense anyway for directories: add a
      precondition, and store internally the null_unbounded_string in the
      base_name field when Object is a directory.
    - Common_Prefix: actually look for the biggest common path rather than
      the biggest pathname, and always return directories.
    - Containing_Directory: add a Pre to not use it with the Root directory, and
      fix implementation so that we can get the parent directory of a directory.
    - Exists: take into account paths that don't have full path name, and the
      special implicit_project path.
    - Is_Root_Dir: new.
    - Name: do not remove extension for directories
    - Relative_Path: remove local Dir_Sep_Set and use the global Dir_Seps.
      Change missleading parameter name "To" to "From" as this parameter is
      the starting point for the relative path.
    - Path_Name: do not return '/' for root directorioes (violates the
      GPR2.Simple_Name type), but add a pre-condition for not using it on root
      dirs instead.
    - Unchecked_Value: take care of the root directory, else the call to Value
      fails, as Full_Name can't be empty.
    
    Overall, add pre to prevent calls with the Implicit_Project as parameter
    when the result would be not applicable: Name, Value, Base_Name,
    Base_Filename, etc.
    
    Prevent calls to Base_Name and Base_Filename with directories.
    
    Prevent calls to Simple_Name and Containing_Directory for the root
    directory.
    
    Adjust gprclean accordingly (and discovered a bug by doing so):
    gprclean was composing "lib" + View.Path_Name.Base_Name to check for
    temporary lib files generated by gprbuild.
    
    Looking at gpr1 code, the composition should be lib + View.Name, but also
    this assumed Path_Name is a file, which is not true for default projects.
    
    TN: V428-034
    Change-Id: I6d922dc505a264b306a9ab6b0f34d9da7ff8a8a4

--- testsuite/tests/common-prefix/main.adb
+++ testsuite/tests/common-prefix/main.adb
@@ -30,8 +30,10 @@ procedure Main is
    P2 : constant Path_Name.Object :=
           Path_Name.Create_File ("/dir1/dir2/dir3/toto");
    P3 : constant Path_Name.Object :=
-          Path_Name.Create_File ("/dira/toto");
+          Path_Name.Create_File ("/dir1/dira/toto");
    P4 : constant Path_Name.Object :=
+          Path_Name.Create_Directory ("/dir1/dira/toto");
+   P5 : constant Path_Name.Object :=
           Path_Name.Create_File ("/xyz");
 
    function Remove_Drive_Letter (Path : String) return String is
@@ -51,5 +53,7 @@ begin
    Text_IO.Put_Line
      ("3: " & Remove_Drive_Letter (P3.Common_Prefix (P3).Value));
    Text_IO.Put_Line
-     ("4: " & Remove_Drive_Letter (P3.Common_Prefix (P4).Value));
+     ("4: " & Remove_Drive_Letter (P4.Common_Prefix (P4).Value));
+   Text_IO.Put_Line
+     ("5: " & Remove_Drive_Letter (P3.Common_Prefix (P5).Value));
 end Main;
---
+++ testsuite/tests/path_name/main.adb
@@ -0,0 +1,124 @@
+with Ada.Environment_Variables;
+with Ada.Strings.Unbounded;
+with Ada.Text_IO;    use Ada.Text_IO;
+
+with GNAT.OS_Lib;
+
+with GPR2.Path_Name; use GPR2.Path_Name;
+
+procedure Main is
+   First_Check : Boolean := True;
+
+   procedure Check_Path (Header : String;
+                         Path   : Object;
+                         Short  : Boolean := False)
+   is
+   begin
+      if not First_Check then
+         New_Line;
+      end if;
+
+      First_Check := False;
+      Put_Line (Header);
+
+      if not Path.Is_Defined then
+         Put_Line ("- *Undefined*");
+         return;
+      end if;
+
+      if Path.Is_Directory then
+         Put_Line ("- Is a directory");
+      else
+         Put_Line ("- Is a file");
+      end if;
+
+      if Path.Is_Implicit_Project then
+         Put_Line ("- Is Implicit Project");
+      end if;
+
+      if Path.Is_Root_Dir then
+         Put_Line ("- Root directory");
+      end if;
+
+      if Path.Exists then
+         Put_Line ("- Actually exists");
+      else
+         Put_Line ("- Does not exist on disk");
+      end if;
+
+      if Short then
+         return;
+      end if;
+
+      if Path.Has_Dir_Name then
+         Put_Line ("- Dir_Name: " & String (Path.Dir_Name));
+      end if;
+
+      if Path /= Implicit_Project then
+         Put_Line ("- Name: " & String (Path.Name));
+         Put_Line ("- Name (no extension): " & String (Path.Name (False)));
+         if Path.Has_Value then
+            Put_Line ("- Value: " & String (Path.Value));
+         end if;
+
+         if not Path.Is_Directory then
+            Put_Line ("- Base_Name: " & String (Path.Base_Name));
+            Put_Line ("- Base_Filename: " & String (Path.Base_Filename));
+         end if;
+
+         if not Path.Is_Root_Dir then
+            Put_Line ("- Simple_Name: " & String (Path.Simple_Name));
+            Put_Line ("- Containing_Directory: " & String (Path.Containing_Directory.Name));
+         end if;
+      end if;
+   end Check_Path;
+
+   Base       : constant Object := Create_Directory ("files");
+   Subdir     : constant Object := Base.Compose ("subdir", True);
+   On_Windows : constant Boolean := GNAT.OS_Lib.Directory_Separator = '\';
+
+begin
+   Check_Path ("Root dir:", Create_Directory ("/"));
+   Check_Path ("'.' dir:", Subdir.Compose (".", True));
+   Check_Path ("'..' dir:", Subdir.Compose ("..", True));
+   Check_Path ("Implicit_Project constant:", Implicit_Project);
+   Check_Path ("Non existing file:", Base.Compose ("foo.ext"));
+   Check_Path ("existing file:", Subdir.Compose ("file.txt"));
+   Check_Path ("File with no extension:", Base.Compose ("foo"));
+   Check_Path ("Dir with extension:", Base.Compose ("readme.foo", True));
+   Check_Path ("With white space:", Base.Compose ("with white/space file.txt"));
+   Check_Path ("Common_Prefix (directory, file)",
+               Common_Prefix (Base.Compose ("somedir", True),
+                              Subdir.Compose ("foo.txt")));
+   Check_Path ("Common_Prefix (file, directory)",
+               Common_Prefix (Subdir.Compose ("foo.txt"),
+                              Base.Compose ("somedir", True)));
+   Check_Path ("Common_Prefix (src, src/source.adb)",
+               Common_Prefix (Create_Directory ("src"),
+                              Create_File ("src/source.adb")));
+   Check_Path ("Common_Prefix (src/source.adb, src)",
+               Common_Prefix (Create_File ("src/source.adb"),
+                              Create_Directory ("src")));
+   Check_Path ("Common_Prefix with root common",
+               Common_Prefix (Create_Directory ("/foo/bar"),
+                              Create_File ("/bar/foo/test.txt")));
+   Check_Path ("File with no full path",
+               Create_File ("files/file.txt", No_Resolution));
+   Check_Path ("Check temp directory", Temporary_Directory, True);
+
+   --  Additional windows-specific tests
+   if On_Windows then
+      --  We don't use C:\ because it's filtered out by the testsuite
+      --  infrastructure, making the output hard to decipher...
+      Check_Path ("Root dir D:", Create_Directory ("D:\"));
+      Check_Path ("Common_Prefix (D:\foo\, E:\bar\)",
+                  Common_Prefix (Create_Directory ("D:\foo\"),
+                                 Create_Directory ("E:\bar\")));
+      Check_Path ("Relative_Path (D:\foo\, E:\bar\)",
+                  Relative_Path (Create_Directory ("D:\foo\"),
+                                 Create_Directory ("E:\bar\")));
+      Check_Path ("Relative_Path (D:\Foo\Bar\, D:\foo\baz\)",
+                  Relative_Path (Create_Directory ("D:\Foo\Bar\"),
+                                 Create_Directory ("D:\foo\baz\")));
+   end if;
+end Main;
