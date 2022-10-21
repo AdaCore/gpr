f401c8f8539fa2c85db3dfbaf70f55d62f4bc97a
Philippe Gil <gil@adacore.com>
2022-10-21 13:24:53 +0200

    Let gprinspect/text results gnat version independent
    TN: VA18-028
    Change-Id: I609656a831157661be45c05c1f7e47229ef864ea

--- testsuite/tests/gprinspect/text/test.py
+++ testsuite/tests/gprinspect/text/test.py
@@ -20,6 +20,7 @@ def test(cmd):
     output = re.sub(r"(generated-on.*:) .*", r"\1 2000-01-01 00:00:00", output)
     output = re.sub(r"(Version *:) .*", r"\1 Pro 1.0 (20000101) (<host>)", output)
     output = re.sub(r"(version.*:) .*", r"\1 Pro 1.0 (20000101) (<host>)", output)
+    output = re.sub(r"(.*value.*GNAT ).*", r"\1<version>", output)
     output = output.replace(os.getcwd(), "<cwd>")
     # json mode uses // as path separator
     output = output.replace(re.sub(r"//", r"/", os.getcwd()), "<cwd>")
