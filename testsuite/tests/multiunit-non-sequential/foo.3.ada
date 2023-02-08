fa8bdfb01f6a30ef6fd3ae14283d4039e80ffdfa
Fedor Rybin <frybin@adacore.com>
2023-02-08 23:23:30 +0300

    Fix crash on absent pos in custom naming
    Improve iteration on GPR2.Unit.List.Object to properly skip
    empty padding for cases when some of positions
    in the beginning/middle of multi-unit range are not used.
    
    TN: W110-002, V817-013
    
    Closes eng/gpr/gpr#25

---
+++ testsuite/tests/multiunit-non-sequential/foo.3.ada
@@ -0,0 +1,6 @@
+package Foo is end Foo;
+package Bar is end Bar;
+package Car is end Car;
+package Tar is end Tar;
+package Par is end Par;
+package Dar is end Dar;
---
+++ testsuite/tests/multiunit-non-sequential/main.adb
@@ -0,0 +1,34 @@
+with GPR2.Log;
+with GPR2.Project.Tree;
+with GPR2.Context;
+with GPR2.Path_Name;
+
+with Ada.Text_IO;
+
+procedure Main is
+   Tree : GPR2.Project.Tree.Object;
+begin
+   Tree.Load_Autoconf
+     (Filename => GPR2.Path_Name.Create_File ("g.gpr"),
+      Context  => GPR2.Context.Empty);
+   Tree.Update_Sources;
+
+      for S of Tree.Root_Project.Sources loop
+      Ada.Text_IO.Put_Line (String (S.Path_Name.Simple_Name));
+      for CU of S.Units loop
+         Ada.Text_IO.Put_Line ("  - compilation unit at" & CU.Index'Image);
+         Ada.Text_IO.Put_Line ("    unit name    = " & String (CU.Name));
+         Ada.Text_IO.Put_Line ("    kind         = " & CU.Kind'Image);
+      end loop;
+   end loop;
+
+   Tree.Unload;
+
+exception
+   when GPR2.Project_Error =>
+      For M in Tree.Log_Messages.Iterate (Information => False) loop
+         Ada.Text_IO.Put_Line (GPR2.Log.Element (M).Format);
+      end loop;
+      Tree.Unload;
+
+end Main;
