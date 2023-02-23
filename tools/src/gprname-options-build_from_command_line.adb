6fb853d63f461bcc3d2899f3b1201213001b8638
Pierre Béguet <pbeguet@adacore.com>
2023-02-23 10:58:53 +0100

    Fixes definitely lost memory using GPRTools
    After making GPRTools available with debug symbol, fixes the way
    we handle command line arguments in GPRname.
    
    Issue: #16

--- tools/src/gprname-options-build_from_command_line.adb
+++ tools/src/gprname-options-build_from_command_line.adb
@@ -18,6 +18,7 @@
 
 with Ada.Command_Line;
 with Ada.Directories;
+with Ada.Exceptions;
 with Ada.Text_IO;
 
 with GNAT.OS_Lib;
@@ -195,183 +196,194 @@ begin
    --       allowed. It is consistent with the syntax for '=' (e.g. --RTS=...).
    --     - Support is added for foreign language excluded patterns.
 
-   while Pos <= Argument_Count loop
-      Arg := new String'(Argument (Pos));
+   Loop_Through_Args : begin
+      while Pos <= Argument_Count loop
+         Arg := new String'(Argument (Pos));
 
-      if Arg'Length >= 2 and then Arg (1 .. 2) = "-P" then
-         if Self.Project_File /= No_String then
-            raise GPRname_Exception with "only one -P switch may be specified";
-         elsif Arg'Length = 2 then
-            Read_Next_Arg (Err_Mssg => "project file name missing",
-                           Dash_Allowed => False);
-            Self.Project_File := +(Arg.all);
-         else
-            Self.Project_File := +(Arg (3 .. Arg'Last));
-         end if;
+         if Arg'Length >= 2 and then Arg (1 .. 2) = "-P" then
+            if Self.Project_File /= No_String then
+               raise GPRname_Exception
+                 with "only one -P switch may be specified";
+            elsif Arg'Length = 2 then
+               Read_Next_Arg (Err_Mssg     => "project file name missing",
+                              Dash_Allowed => False);
+               Self.Project_File := +(Arg.all);
+            else
+               Self.Project_File := +(Arg (3 .. Arg'Last));
+            end if;
 
-         Root_Prj := +Containing_Directory (To_String (Self.Project_File));
+            Root_Prj := +Containing_Directory (To_String (Self.Project_File));
 
-      elsif Arg.all = "--ignore-predefined-units" then
-         Self.Ignore_Predefined_Units := True;
+         elsif Arg.all = "--ignore-predefined-units" then
+            Self.Ignore_Predefined_Units := True;
 
-      elsif Arg.all = "--ignore-duplicate-files" then
-         Self.Ignore_Duplicate_Files := True;
+         elsif Arg.all = "--ignore-duplicate-files" then
+            Self.Ignore_Duplicate_Files := True;
 
-      elsif Arg.all = "--no-backup" then
-         Self.No_Backup := True;
+         elsif Arg.all = "--no-backup" then
+            Self.No_Backup := True;
 
-      elsif Arg'Length >= 5 and then Arg (1 .. 5) = "--RTS" then
-         if Self.RTS /= No_String then
-            raise GPRname_Exception
-              with "--RTS cannot be specified multiple times";
-         elsif Arg'Length <= 6 or else Arg (6) /= '=' then
-            raise GPRname_Exception with "missing path for --RTS";
-         else
-            Self.RTS := +Arg (7 .. Arg'Last);
-         end if;
+         elsif Arg'Length >= 5 and then Arg (1 .. 5) = "--RTS" then
+            if Self.RTS /= No_String then
+               raise GPRname_Exception
+                 with "--RTS cannot be specified multiple times";
+            elsif Arg'Length <= 6 or else Arg (6) /= '=' then
+               raise GPRname_Exception with "missing path for --RTS";
+            else
+               Self.RTS := +Arg (7 .. Arg'Last);
+            end if;
 
-      elsif Arg'Length >= 8 and then Arg (1 .. 8) = "--target" then
-         if Self.Target /= No_String then
-            raise GPRname_Exception
-              with "--target cannot be specified multiple times";
-         elsif Arg'Length <= 9 or else Arg (9) /= '=' then
-            raise GPRname_Exception with "missing path for --target";
-         else
-            Self.Target := +Arg (10 .. Arg'Last);
-         end if;
+         elsif Arg'Length >= 8 and then Arg (1 .. 8) = "--target" then
+            if Self.Target /= No_String then
+               raise GPRname_Exception
+                 with "--target cannot be specified multiple times";
+            elsif Arg'Length <= 9 or else Arg (9) /= '=' then
+               raise GPRname_Exception with "missing path for --target";
+            else
+               Self.Target := +Arg (10 .. Arg'Last);
+            end if;
 
-      elsif Arg.all = "-eL" then
-         Self.Follow_Symlinks := True;
+         elsif Arg.all = "-eL" then
+            Self.Follow_Symlinks := True;
 
-      elsif Arg.all = "-v" then
-         Show_Version;
-         if Self.Verbosity = None then
-            Self.Verbosity := Low;
-         elsif Self.Verbosity = Low then
-            Self.Verbosity := High;
-         end if;
+         elsif Arg.all = "-v" then
+            Show_Version;
+            if Self.Verbosity = None then
+               Self.Verbosity := Low;
+            elsif Self.Verbosity = Low then
+               Self.Verbosity := High;
+            end if;
 
-      elsif Arg'Length = 4 and then Arg (1 .. 3) = "-vP"
-        and then Arg (4) in '0' .. '2'
-      then
-         Self.Verbose_Parsing := Integer'Value (Arg (4 .. 4));
+         elsif Arg'Length = 4 and then Arg (1 .. 3) = "-vP"
+           and then Arg (4) in '0' .. '2'
+         then
+            Self.Verbose_Parsing := Integer'Value (Arg (4 .. 4));
 
-      elsif Arg.all = "-h" or else Arg.all = "--help" then
-         Usage;
-         GNAT.OS_Lib.OS_Exit (0);
+         elsif Arg.all = "-h" or else Arg.all = "--help" then
+            Usage;
+            GNAT.OS_Lib.OS_Exit (0);
 
-      elsif Arg.all = "--version" then
-         Show_Version;
-         GPR2.Version.Display_Free_Software;
-         GNAT.OS_Lib.OS_Exit (0);
+         elsif Arg.all = "--version" then
+            Show_Version;
+            GPR2.Version.Display_Free_Software;
+            GNAT.OS_Lib.OS_Exit (0);
 
-      elsif Arg.all = "--minimal-dirs" then
-         Self.Minimal_Dirs := True;
+         elsif Arg.all = "--minimal-dirs" then
+            Self.Minimal_Dirs := True;
 
-      elsif Arg'Length >= 8 and then
-        (Arg  (1 .. 7) = "-gnatep" or else Arg  (1 .. 7) = "-gnateD")
-      then
-         Self.Prep_Switches.Append (Arg.all);
+         elsif Arg'Length >= 8 and then
+           (Arg  (1 .. 7) = "-gnatep" or else Arg  (1 .. 7) = "-gnateD")
+         then
+            Self.Prep_Switches.Append (Arg.all);
 
-      elsif Arg.all = "--and" then
-         Prepare_And_Add_Section (Current_Section);
-         Current_Section.Reset;
+         elsif Arg.all = "--and" then
+            Prepare_And_Add_Section (Current_Section);
+            Current_Section.Reset;
 
-      elsif Arg'Length >= 2 and then Arg (1 .. 2) = "-d" then
-         if Arg'Length = 2 then
-            Read_Next_Arg (Err_Mssg => "directory name missing");
-            Add_Directory (Arg.all);
-         else
-            Add_Directory (Arg.all (3 .. Arg'Last));
-         end if;
+         elsif Arg'Length >= 2 and then Arg (1 .. 2) = "-d" then
+            if Arg'Length = 2 then
+               Read_Next_Arg (Err_Mssg => "directory name missing");
+               Add_Directory (Arg.all);
+            else
+               Add_Directory (Arg.all (3 .. Arg'Last));
+            end if;
 
-      elsif Arg'Length >= 2 and then Arg (1 .. 2) = "-D" then
-         if Arg'Length = 2 then
-            Read_Next_Arg (Err_Mssg => "directories file name missing");
-            Current_Section.Add_Directories_File (Arg.all);
-         else
-            Current_Section.Add_Directories_File (Arg.all (3 .. Arg'Last));
-         end if;
+         elsif Arg'Length >= 2 and then Arg (1 .. 2) = "-D" then
+            if Arg'Length = 2 then
+               Read_Next_Arg (Err_Mssg => "directories file name missing");
+               Current_Section.Add_Directories_File (Arg.all);
+            else
+               Current_Section.Add_Directories_File (Arg.all (3 .. Arg'Last));
+            end if;
 
-      elsif Arg'Length >= 2 and then Arg (1 .. 2) = "-f" then
+         elsif Arg'Length >= 2 and then Arg (1 .. 2) = "-f" then
 
-         if Arg'Length = 2 then
-            Read_Next_Arg (Err_Mssg => "pattern missing for language C");
-            Current_Section.Add_Language_Pattern
-              (Language_Type'("C"), Pattern_Type (Arg.all));
-
-         elsif Arg (3) /= ':' then
-            Current_Section.Add_Language_Pattern
-              (Language_Type'("C"), Pattern_Type (Arg (3 .. Arg'Last)));
-
-         elsif Arg'Length > 3 then
-            declare
-               Lang : constant String := Arg (4 .. Arg'Last);
-            begin
-               Read_Next_Arg
-                 (Err_Mssg => "pattern missing for language " & Lang);
+            if Arg'Length = 2 then
+               Read_Next_Arg (Err_Mssg => "pattern missing for language C");
                Current_Section.Add_Language_Pattern
-                 (Language_Type (Lang), Pattern_Type (Arg.all));
-            end;
+                 (Language_Type'("C"), Pattern_Type (Arg.all));
 
-         else
-            raise GPRname_Exception with "language missing for -f:";
-         end if;
+            elsif Arg (3) /= ':' then
+               Current_Section.Add_Language_Pattern
+                 (Language_Type'("C"), Pattern_Type (Arg (3 .. Arg'Last)));
 
-      elsif Arg'Length >= 2 and then Arg (1 .. 2) = "-x" then
+            elsif Arg'Length > 3 then
+               declare
+                  Lang : constant String := Arg (4 .. Arg'Last);
+               begin
+                  Read_Next_Arg
+                    (Err_Mssg => "pattern missing for language " & Lang);
+                  Current_Section.Add_Language_Pattern
+                    (Language_Type (Lang), Pattern_Type (Arg.all));
+               end;
 
-         if Arg'Length = 2 then
-            Read_Next_Arg (Err_Mssg => "excluded pattern missing");
-            Current_Section.Add_Excluded_Language_Pattern
-              (Language_Type'("Ada"), Pattern_Type (Arg.all));
+            else
+               raise GPRname_Exception with "language missing for -f:";
+            end if;
 
-         elsif Arg (3) /= 'f' then
-            Current_Section.Add_Excluded_Language_Pattern
-              (Language_Type'("Ada"), Pattern_Type (Arg.all (3 .. Arg'Last)));
+         elsif Arg'Length >= 2 and then Arg (1 .. 2) = "-x" then
 
-         elsif Arg'Length = 3 then
-            Read_Next_Arg
-              (Err_Mssg => "excluded pattern missing for language C");
-            Current_Section.Add_Excluded_Language_Pattern
-              (Language_Type'("C"), Pattern_Type (Arg.all));
-
-         elsif Arg (4) /= ':' then
-            Current_Section.Add_Excluded_Language_Pattern
-              (Language_Type'("C"), Pattern_Type (Arg (4 .. Arg'Last)));
-
-         elsif Arg'Length > 4 then
-            declare
-               Lang : constant String := Arg (5 .. Arg'Last);
-            begin
-               Read_Next_Arg
-                 (Err_Mssg => "excluded pattern missing for language " & Lang);
+            if Arg'Length = 2 then
+               Read_Next_Arg (Err_Mssg => "excluded pattern missing");
                Current_Section.Add_Excluded_Language_Pattern
-                 (Language_Type (Lang), Pattern_Type (Arg.all));
-            end;
+                 (Language_Type'("Ada"), Pattern_Type (Arg.all));
+
+            elsif Arg (3) /= 'f' then
+               Current_Section.Add_Excluded_Language_Pattern
+                 (Language_Type'("Ada"),
+                  Pattern_Type (Arg.all (3 .. Arg'Last)));
+
+            elsif Arg'Length = 3 then
+               Read_Next_Arg
+                 (Err_Mssg => "excluded pattern missing for language C");
+               Current_Section.Add_Excluded_Language_Pattern
+                 (Language_Type'("C"), Pattern_Type (Arg.all));
+
+            elsif Arg (4) /= ':' then
+               Current_Section.Add_Excluded_Language_Pattern
+                 (Language_Type'("C"), Pattern_Type (Arg (4 .. Arg'Last)));
+
+            elsif Arg'Length > 4 then
+               declare
+                  Lang : constant String := Arg (5 .. Arg'Last);
+               begin
+                  Read_Next_Arg
+                    (Err_Mssg => "excluded pattern missing for language "
+                                 & Lang);
+                  Current_Section.Add_Excluded_Language_Pattern
+                    (Language_Type (Lang), Pattern_Type (Arg.all));
+               end;
+            else
+               raise GPRname_Exception with "language missing for -xf:";
+            end if;
+
+         elsif Arg (1) = '-' then
+
+            --  Any isolated argument starting with a dash is interpreted as a
+            --  switch. To add a pattern starting with '-' for Ada, use the
+            --  explicit form: -f:Ada <pat>
+
+            raise GPRname_Exception with "wrong switch: " & Arg.all;
+
          else
-            raise GPRname_Exception with "language missing for -xf:";
+            --  Pattern for Ada
+
+            Current_Section.Add_Language_Pattern
+              (Language_Type'("Ada"), Pattern_Type (Arg.all));
          end if;
 
-      elsif Arg (1) = '-' then
+         --  Go to the next argument
 
-         --  Any isolated argument starting with a dash is interpreted as a
-         --  switch. To add a pattern starting with '-' for Ada, use the
-         --  explicit form: -f:Ada <pat>
-
-         raise GPRname_Exception with "wrong switch: " & Arg.all;
-
-      else
-         --  Pattern for Ada
-
-         Current_Section.Add_Language_Pattern
-           (Language_Type'("Ada"), Pattern_Type (Arg.all));
-      end if;
-
-      --  Go to the next argument
-
-      Free (Arg);
-      Pos := Pos + 1;
-   end loop;
+         Free (Arg);
+         Pos := Pos + 1;
+      end loop;
+   exception
+      when E : others =>
+         if Arg /= null then
+            Free (Arg);
+         end if;
+         raise GPRname_Exception with Ada.Exceptions.Exception_Message (E);
+   end Loop_Through_Args;
 
    --  Check that we have a (non-empty) project file name
 
