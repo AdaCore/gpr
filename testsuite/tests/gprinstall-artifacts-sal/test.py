a5dfb3b1e09a7f5b4d6c775a0231c981b70b4a07
Pascal Obry <obry@adacore.com>
2023-03-30 17:46:42 +0200

    Fix GPRinstall test for SAL artifacts on Windows.
    Contnued for for VC03-001.
    Part of #10.

--- testsuite/tests/gprinstall-artifacts-sal/test.py
+++ testsuite/tests/gprinstall-artifacts-sal/test.py
@@ -1,5 +1,8 @@
 import os
 import subprocess
+from testsuite_support.builder_and_runner import BuilderAndRunner
+
+bnr = BuilderAndRunner()
 
 # GPRbuild
 subprocess.run("gprbuild -p -q libtst.gpr", shell=True)
@@ -10,7 +13,7 @@ subprocess.run("gprinstall --prefix=installm -m -p libtst.gpr", shell=True)
 
 # Test installation
 subprocess.run("gprbuild -q -f -aPinstall/share/gpr use/usel.gpr", shell=True)
-subprocess.run("./use/obj/main", shell=True)
+bnr.run(["./use/obj/main"], output=True)
 
 subprocess.run("gprbuild -q -f -aPinstallm/share/gpr use/usel.gpr", shell=True)
-subprocess.run("./use/obj/main", shell=True)
+bnr.run(["./use/obj/main"], output=True)
