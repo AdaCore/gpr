14d7a113629cfd6d40e939018b1823f1109dbe1e
Jerome Lambourg <lambourg@adacore.com>
2023-05-23 16:38:20 +0200

    gpr2next: fix test "extended"
    For eng/gpr/gpr-issues#67

--- testsuite/tests/extended/main.adb
+++ testsuite/tests/extended/main.adb
@@ -7,15 +7,12 @@
 with Ada.Text_IO;
 with Ada.Strings.Fixed;
 
-with GPR2.Unit;
+with GPR2.Build.Source.Sets;
 with GPR2.Context;
+with GPR2.Log;
 with GPR2.Path_Name;
-with GPR2.Project.Source.Set;
 with GPR2.Project.View;
 with GPR2.Project.Tree;
-with GPR2.Source;
-
-with GPR2.Source_Info.Parser.Ada_Language;
 
 procedure Main is
 
@@ -44,26 +41,27 @@ procedure Main is
 
       Text_IO.Put_Line (Prj.Qualifier'Img);
 
-      for Source of Prj.Sources loop
-         declare
-            U : constant Optional_Name_Type := Source.Unit_Name;
-         begin
-            Output_Filename (Source.Path_Name.Value);
+      if Prj.Kind in With_Object_Dir_Kind then
+         for Source of Prj.Sources loop
+            declare
+               U : constant Optional_Name_Type := Source.Unit.Name;
+            begin
+               Output_Filename (Source.Path_Name.Value);
 
-            Text_IO.Set_Col (16);
-            Text_IO.Put ("   language: " & Image (Source.Language));
+               Text_IO.Set_Col (16);
+               Text_IO.Put ("   language: " & Image (Source.Language));
 
-            Text_IO.Set_Col (33);
-            Text_IO.Put ("   Kind: "
-                         & GPR2.Unit.Library_Unit_Type'Image (Source.Kind));
+               Text_IO.Set_Col (33);
+               Text_IO.Put ("   Kind: " & Source.Kind'Image);
 
-            if U /= "" then
-               Text_IO.Put ("   unit: " & String (U));
-            end if;
+               if U /= "" then
+                  Text_IO.Put ("   unit: " & String (U));
+               end if;
 
-            Text_IO.New_Line;
-         end;
-      end loop;
+               Text_IO.New_Line;
+            end;
+         end loop;
+      end if;
    end Display;
 
    ---------------------
@@ -78,9 +76,17 @@ procedure Main is
 
    Prj1, Prj2 : Project.Tree.Object;
    Ctx        : Context.Object;
+   Log        : GPR2.Log.Object;
 begin
    Project.Tree.Load (Prj1, Project.Create ("prj1.gpr"), Ctx);
+   Prj1.Log_Messages.Output_Messages (Information => False);
+   Prj1.Update_Sources (Messages => Log);
+   Log.Output_Messages;
+
    Project.Tree.Load (Prj2, Project.Create ("prj2.gpr"), Ctx);
+   Prj2.Log_Messages.Output_Messages (Information => False);
+   Prj2.Update_Sources (Messages => Log);
+   Log.Output_Messages;
 
    Text_IO.Put_Line ("**************** Iterator Prj1");
 
