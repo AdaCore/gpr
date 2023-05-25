53847f7d2a0a4fdd1a6e786585ba9498694a0a35
Vincent Jicquel <jicquel@adacore.com>
2023-05-25 14:42:52 +0200

    gpr2next: add analysis for test "tree-get-file-extending"
    GPR2.Project_Tree.Get_File does not exist anymore.
    
    For eng/gpr/gpr-issues#67

--- testsuite/tests/tree-get-file-extending/main.adb
+++ testsuite/tests/tree-get-file-extending/main.adb
@@ -7,6 +7,7 @@
 with Ada.Text_IO;
 with GPR2.Context;
 with GPR2.Path_Name;
+with GPR2.Log;
 with GPR2.Project.Tree;
 
 procedure Main is
@@ -14,14 +15,17 @@ procedure Main is
    Context      : GPR2.Context.Object;
    Project_Name : constant GPR2.Filename_Type := "extending";
    use GPR2;
+   Log  : GPR2.Log.Object;
 begin
    Tree.Load_Autoconf
-     (Filename => GPR2.Path_Name.Create_File
+     (Filename     => GPR2.Path_Name.Create_File
         (GPR2.Project.Ensure_Extension (Project_Name),
          GPR2.Path_Name.No_Resolution),
-      Context  => Context);
+      Context      => Context,
+      With_Runtime => True);
 
-   Tree.Update_Sources (With_Runtime => True);
+   Tree.Update_Sources (Messages => Log);
+   Log.Output_Messages (Information => False);
 
    declare
       use Ada.Text_IO;
