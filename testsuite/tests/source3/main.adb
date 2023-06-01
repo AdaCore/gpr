6e9fcb8cf399c1712e578ccd494d75e2423479f1
Jerome Lambourg <lambourg@adacore.com>
2023-06-01 20:22:40 +0200

    gpr2next: fix test "source3"
    For eng/gpr/gpr-issues#67

--- testsuite/tests/source3/main.adb
+++ testsuite/tests/source3/main.adb
@@ -7,19 +7,18 @@
 with Ada.Strings.Fixed;
 with Ada.Text_IO;
 
-with GPR2.Unit;
+with GPR2.Build.Source.Sets;
 with GPR2.Context;
+with GPR2.Log;
 with GPR2.Path_Name;
-with GPR2.Project.Source.Set;
 with GPR2.Project.View;
 with GPR2.Project.Tree;
-
-with GPR2.Source_Info.Parser.Ada_Language;
 
 procedure Main is
 
    use Ada;
    use GPR2;
+   use GPR2.Build;
    use GPR2.Project;
 
    procedure Check (Project_Name : Filename_Type);
@@ -36,15 +35,20 @@ procedure Main is
       Prj  : Project.Tree.Object;
       Ctx  : Context.Object;
       View : Project.View.Object;
+      Log  : GPR2.Log.Object;
    begin
       Project.Tree.Load (Prj, Create (Project_Name), Ctx);
 
       View := Prj.Root_Project;
       Text_IO.Put_Line ("Project: " & String (View.Name));
 
+      Prj.Update_Sources (Messages => Log);
+      Log.Output_Messages;
+
       for Source of View.Sources loop
          declare
-            U : constant Optional_Name_Type := Source.Unit_Name;
+            U : constant Optional_Name_Type :=
+                  GPR2.Build.Source.Full_Name (Source.Unit);
          begin
             Output_Filename (Source.Path_Name.Value);
 
@@ -52,8 +56,7 @@ procedure Main is
             Text_IO.Put ("   language: " & Image (Source.Language));
 
             Text_IO.Set_Col (36);
-            Text_IO.Put ("   Kind: "
-                         & GPR2.Unit.Library_Unit_Type'Image (Source.Kind));
+            Text_IO.Put ("   Kind: " & Source.Kind'Image);
 
             if U /= "" then
                Text_IO.Set_Col (60);
