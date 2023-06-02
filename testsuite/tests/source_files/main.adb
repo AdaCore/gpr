61dddc40c50688914b10ffdf2e31a374fe55f0e3
Jerome Lambourg <lambourg@adacore.com>
2023-06-02 10:03:07 +0200

    gpr2next: fix test "source_files"
    For eng/gpr/gpr-issues#67

--- testsuite/tests/source_files/main.adb
+++ testsuite/tests/source_files/main.adb
@@ -10,14 +10,12 @@ with Ada.Strings.Fixed;
 
 with GNAT.OS_Lib;
 
+with GPR2.Build.Source.Sets;
 with GPR2.Context;
 with GPR2.Log;
 with GPR2.Message;
 with GPR2.Path_Name;
-with GPR2.Project.Source;
-with GPR2.Project.Source.Set;
 with GPR2.Project.Tree;
-with GPR2.Source;
 
 procedure Main is
 
@@ -25,10 +23,10 @@ procedure Main is
    use GPR2;
    use GPR2.Project;
 
-   procedure Display_Source (Src : GPR2.Source.Object'Class);
+   procedure Display_Source (Src : GPR2.Build.Source.Object);
    procedure Test_Prj (Fname : Filename_Type);
 
-   procedure Display_Source (Src : GPR2.Source.Object'Class) is
+   procedure Display_Source (Src : GPR2.Build.Source.Object) is
    begin
       Text_IO.Put_Line (String (Src.Path_Name.Simple_Name) & ": " & Src.Kind'Image);
    end Display_Source;
@@ -37,50 +35,26 @@ procedure Main is
    is
       Prj : Project.Tree.Object;
       Ctx : Context.Object;
+      Log : GPR2.Log.Object;
 
    begin
       Text_IO.Put_Line ("GPR file: " & String (Fname));
       Project.Tree.Load (Prj, Create (Fname), Ctx);
-      begin
-         Prj.Update_Sources;
-         for V of reverse Prj.Ordered_Views loop
-            Text_IO.Put_Line (String (V.Name));
-            for S of V.Sources loop
-               Display_Source (S);
-            end loop;
-         end loop;
-      exception
-         when Project_Error =>
-            Text_IO.Put_Line ("Messages found:");
+      Prj.Update_Sources (Messages => Log);
+      if Log.Has_Element then
+         Text_IO.Put_Line ("Messages found:");
+         Log.Output_Messages;
+      end if;
 
-            for C in Prj.Log_Messages.Iterate
-              (False, True, True, True, True)
-            loop
-               declare
-                  use Ada.Strings;
-                  use Ada.Strings.Fixed;
-                  DS  : Character renames GNAT.OS_Lib.Directory_Separator;
-                  M   : constant Message.Object := Log.Element (C);
-                  Mes : constant String := M.Format;
-                  L   : constant Natural :=
-                          Fixed.Index (Mes, DS & "source_files" & DS);
-               begin
-                  if L /= 0 then
-                     Text_IO.Put_Line
-                       (Replace_Slice
-                          (Mes,
-                           Fixed.Index
-                             (Mes (1 .. L), """", Going => Backward) + 1,
-                           L - 1,
-                           "<path>"));
-                  else
-                     Text_IO.Put_Line (Mes);
-                  end if;
-               end;
-            end loop;
-      end;
+      for V of reverse Prj.Ordered_Views loop
+         Text_IO.Put_Line (String (V.Name));
+         for S of V.Sources loop
+            Display_Source (S);
+         end loop;
+      end loop;
 
       Prj.Unload;
+      Text_IO.New_Line;
    end Test_Prj;
 
 begin
