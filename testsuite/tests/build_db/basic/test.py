dba2d49b31d04bda35413927a18a83f5179ad489
Jerome Lambourg <lambourg@adacore.com>
2023-09-22 10:27:44 +0200

    Fix View_Db.Visible_Source when an abstract project is present.
    Visible_Source was iterating on all project's views and retrieving the
    view database without checking if they could have an object directory.
    
    Fix eng/gpr/gpr-issues#148

--- testsuite/tests/build_db/basic/test.py
+++ testsuite/tests/build_db/basic/test.py
@@ -6,7 +6,7 @@ bnr.build(project='prj.gpr',
           args=["-g1", "-q", "-p", "-bargs", "-Es"],
           env={})
 
-bnr.call(['./main', 'trees/agg/agg.gpr'])
+bnr.call(['./main', 'trees/agg/agg.gpr', 'pkg.ads', 'not_found.txt'])
 bnr.call(['./main', 'trees/agg2/agg.gpr'])
 bnr.call(['./main', 'trees/multi/prj.gpr'])
 bnr.call(['./main', 'trees/invalid_naming/prj.gpr'])
