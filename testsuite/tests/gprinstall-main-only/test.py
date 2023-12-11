70f86e5580ab2fb0a758043bcbe2759903bdf8ac
Philippe Gil <gil@adacore.com>
2023-12-11 13:38:59 +0100

    Fix grp2 tests regressions due to python version update
    For eng/gpr/gpr-issues#211

--- testsuite/tests/gprinstall-main-only/test.py
+++ testsuite/tests/gprinstall-main-only/test.py
@@ -4,7 +4,7 @@ from e3.env import Env
 from e3.fs import cp
 from testsuite_support.builder_and_runner import BuilderAndRunner, GPRINSTALL
 
-if Env().host.platform.endswith('windows'):
+if 'windows' in Env().host.platform:
     exeext = '.exe'
     cp('adactl', 'adactl' + exeext)
 else:
