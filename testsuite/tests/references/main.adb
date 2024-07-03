cd2043d416afc68d5204e4db9e49d30921d68cb1
Vadim Godunko <godunko@adacore.com>
2024-07-03 11:15:43 +0400

    Update test driver to "next" API.

--- testsuite/tests/references/main.adb
+++ testsuite/tests/references/main.adb
@@ -1,30 +1,44 @@
+
 with Ada.Text_IO; use Ada.Text_IO;
 
-with GPR2.Context;
+with GPR2.Environment;
 with GPR2.Log;
-with GPR2.Message;
-with GPR2.Path_Name;
+with GPR2.Options;
 with GPR2.Project.Tree;
 with GPR2.Project.Variable;
 
 procedure Main is
+
    Tree : GPR2.Project.Tree.Object;
+   Opt  : GPR2.Options.Object;
+   Env  : GPR2.Environment.Object;
+
+   ----------
+   -- Test --
+   ----------
 
    procedure Test (Name : GPR2.Name_Type) is
       Variable : GPR2.Project.Variable.Object;
+
    begin
       if Tree.Root_Project.Has_Variables (Name) then
          Variable := Tree.Root_Project.Variable (Name);
          Ada.Text_IO.Put_Line (Tree.Root_Project.Variable (Name).Value.Text);
+
       else
          Ada.Text_IO.Put_Line ("Cannot test: " & String (Name));
       end if;
    end Test;
+
 begin
-   GPR2.Project.Tree.Load_Autoconf
-     (Self              => Tree,
-      Filename          => GPR2.Path_Name.Create_File ("references.gpr"),
-      Context           => GPR2.Context.Empty);
+   Opt.Add_Switch (GPR2.Options.P, "references.gpr");
+
+   if not Tree.Load
+     (Options          => Opt,
+      Absent_Dir_Error => GPR2.No_Error)
+   then
+      raise Program_Error;
+   end if;
 
    Ada.Text_IO.New_Line;
    Ada.Text_IO.Put_Line ("### type references ###");
