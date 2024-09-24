cab2dfc9e74f6841417df351e2c4ab26de07d548
Nicolas Roche <roche@adacore.com>
2024-09-24 14:21:46 +0200

    Speed up signature loading
    This uses the SAX-like parser in GNATCOLL.JSON to allow raw
    decoding of strings we know have no escaped characters.

--- src/build/gpr2-build-signature.adb
+++ src/build/gpr2-build-signature.adb
@@ -4,10 +4,16 @@
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-Exception
 --
 
+with GNATCOLL.JSON;
+with GNATCOLL.Buffer;
+
 with Ada.Directories; use Ada.Directories;
 with Ada.Text_IO; use Ada.Text_IO;
 
 package body GPR2.Build.Signature is
+
+   package JSON renames GNATCOLL.JSON;
+   package Buffer renames GNATCOLL.Buffer;
 
    ------------------
    -- Add_Artifact --
@@ -48,49 +54,133 @@ package body GPR2.Build.Signature is
 
    function Load (Db_File : Path_Name.Object) return Object
    is
-      File        : File_Type;
-      JSON_Result : Read_Result;
+      use type JSON.JSON_Parser_Event_Kind;
+
       Signature   : Object;
+      Parser      : JSON.JSON_Parser;
+      Data        : Buffer.Reader := Buffer.Open (String (Db_File.Value));
+      Event       : JSON.JSON_Parser_Event;
 
    begin
-      Signature.Clear;
 
-      Text_IO.Open
-        (File => File,
-         Mode => In_File,
-         Name => String (Db_File.Value));
-
-      begin
-         JSON_Result := Read (Get_Line (File));
-         Close (File);
-      exception
-         when End_Error =>
-            Close (File);
-      end;
-
-      if not JSON_Result.Success then
+      --  Signature is an object
+      Event := Parser.Parse_Next (Data => Data);
+      if Event.Kind /= JSON.OBJECT_START then
          return Signature;
       end if;
 
-      declare
-         List : constant JSON_Array :=
-                  Get (JSON_Result.Value, TEXT_SIGNATURE);
-      begin
-         for Obj of List loop
-            declare
-               Art_Uri : constant UTF8_String := Get (Obj, TEXT_URI);
-               Art_Chk : constant UTF8_String := Get (Obj, TEXT_CHECKSUM);
-            begin
-               Signature.Artifacts.Include
-                 (Artifacts.From_Uri (Art_Uri), Hash_Digest (Art_Chk));
-            end;
-         end loop;
-      end;
+      loop
+         Event := Parser.Parse_Next (Data => Data);
 
-      Signature.Stdout := Get (JSON_Result.Value, TEXT_STDOUT);
-      Signature.Stderr := Get (JSON_Result.Value, TEXT_STDERR);
+         --  At this level we expect either object_end or a string value
+         --  (the key of the object field).
 
-      JSON_Result.Value.Finalize;
+         exit when Event.Kind = JSON.OBJECT_END;
+
+         if Event.Kind /= JSON.STRING_VALUE then
+            Signature.Clear;
+
+            return Signature;
+         end if;
+
+         declare
+            Key : String renames Data.Token (Event.First + 1, Event.Last - 1);
+         begin
+
+            if Key = TEXT_SIGNATURE then
+               --  the signature key as an array associated with it
+               Event := Parser.Parse_Next (Data => Data);
+
+               if Event.Kind /= JSON.ARRAY_START then
+                  return Signature;
+               end if;
+
+               Array_Loop : loop
+                  Event := Parser.Parse_Next (Data => Data);
+                  case Event.Kind is
+                     when JSON.ARRAY_END =>
+                        exit Array_Loop;
+                     when JSON.OBJECT_START =>
+                        declare
+                           use JSON;
+                           Checksum : JSON_Parser_Event := (NULL_VALUE, 0, 0);
+                           URI      : JSON_Parser_Event := (NULL_VALUE, 0, 0);
+                           E        : JSON_Parser_Event;
+                        begin
+                           Checksum_Loop : loop
+                              E := Parser.Parse_Next (Data);
+
+                              if E.Kind = JSON.OBJECT_END then
+                                 exit Checksum_Loop;
+                              end if;
+
+                              if E.Kind = JSON.STRING_VALUE then
+                                 declare
+                                    Key : constant String :=
+                                      Data.Token (E.First + 1, E.Last - 1);
+                                 begin
+                                    if Key = TEXT_CHECKSUM then
+                                       Checksum := Parser.Parse_Next (Data);
+                                    elsif Key = TEXT_URI then
+                                       URI := Parser.Parse_Next (Data);
+                                    else
+                                       Signature.Clear;
+                                       return Signature;
+                                    end if;
+                                 end;
+                              else
+                                 Signature.Clear;
+                                 return Signature;
+                              end if;
+                           end loop Checksum_Loop;
+
+                           if Checksum.Kind /= STRING_VALUE or else
+                             URI.Kind /= STRING_VALUE
+                           then
+                              Signature.Clear;
+                              return Signature;
+                           end if;
+
+                           Signature.Artifacts.Include
+                             (Artifacts.From_Uri
+                                (Data.Token
+                                   (URI.First + 1,
+                                    URI.Last - 1)),
+                              Hash_Digest
+                                (Data.Token
+                                   (Checksum.First + 1,
+                                    Checksum.Last - 1)));
+                        end;
+                     when others =>
+                        Signature.Clear;
+                        return Signature;
+                  end case;
+               end loop Array_Loop;
+
+            elsif Key = TEXT_STDOUT or else Key = TEXT_STDERR then
+               Event := Parser.Parse_Next (Data => Data);
+
+               if Event.Kind /= JSON.STRING_VALUE then
+                  return Signature;
+               end if;
+
+               if Key = TEXT_STDOUT then
+                  Signature.Stdout := To_Unbounded_String
+                    (JSON.Decode_As_String (Event, Data));
+               else
+                  Signature.Stderr := To_Unbounded_String
+                    (JSON.Decode_As_String (Event, Data));
+               end if;
+            end if;
+         end;
+      end loop;
+
+      --  Check for end of document
+      Event := Parser.Parse_Next (Data => Data);
+      if Event.Kind /= JSON.DOC_END then
+         Signature.Clear;
+         return Signature;
+      end if;
 
       return Signature;
 
@@ -106,44 +196,44 @@ package body GPR2.Build.Signature is
 
    procedure Store (Self : in out Object; Db_File : Path_Name.Object) is
       File : File_Type;
-      JSON : constant JSON_Value := Create_Object;
-      List : JSON_Array;
+      Value : constant JSON.JSON_Value := JSON.Create_Object;
+      List : JSON.JSON_Array;
 
       procedure Create_Artifact_Element (Position : Artifact_Maps.Cursor);
 
       procedure Create_Artifact_Element (Position : Artifact_Maps.Cursor) is
          Art : constant Artifacts.Object'Class :=
                  Artifact_Maps.Key (Position);
-         Val : constant JSON_Value := Create_Object;
+         Val : constant JSON.JSON_Value := JSON.Create_Object;
       begin
-         Set_Field (Val => Val,
+         JSON.Set_Field (Val => Val,
                     Field_Name => TEXT_URI,
                     Field      => Artifacts.To_Uri (Art));
-         Set_Field (Val => Val,
+         JSON.Set_Field (Val => Val,
                     Field_Name => TEXT_CHECKSUM,
                     Field      => String (Art.Checksum));
-         Append (List, Val);
+         JSON.Append (List, Val);
       end Create_Artifact_Element;
    begin
       Self.Artifacts.Iterate (Create_Artifact_Element'Access);
 
-      Set_Field (Val        => JSON,
-                 Field_Name => TEXT_SIGNATURE,
-                 Field      => List);
-      Set_Field (Val        => JSON,
-                 Field_Name => TEXT_STDOUT,
-                 Field      => Self.Stdout);
-      Set_Field (Val        => JSON,
-                 Field_Name => TEXT_STDERR,
-                 Field      => Self.Stderr);
+      JSON.Set_Field (Val        => Value,
+                      Field_Name => TEXT_SIGNATURE,
+                      Field      => List);
+      JSON.Set_Field (Val        => Value,
+                      Field_Name => TEXT_STDOUT,
+                      Field      => Self.Stdout);
+      JSON.Set_Field (Val        => Value,
+                      Field_Name => TEXT_STDERR,
+                      Field      => Self.Stderr);
 
       if Exists (String (Db_File.Value)) then
          Open (File, Out_File, String (Db_File.Value));
-         Put_Line (File, Write (JSON) & ASCII.CR & ASCII.LF);
+         Put_Line (File, JSON.Write (Value) & ASCII.CR & ASCII.LF);
          Close (File);
       elsif Exists (String (Db_File.Containing_Directory.Value)) then
          Create (File, Out_File, String (Db_File.Value));
-         Put_Line (File, Write (JSON) & ASCII.CR & ASCII.LF);
+         Put_Line (File, JSON.Write (Value) & ASCII.CR & ASCII.LF);
          Close (File);
       end if;
    end Store;
