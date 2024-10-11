a48ff6d2923a5dc09e677dcf09e5fa694d2eb469
Vincent Jicquel <jicquel@adacore.com>
2024-10-11 15:58:05 +0200

    Document Propagate_Actions

--- src/build/gpr2-build-tree_db.ads
+++ src/build/gpr2-build-tree_db.ads
@@ -133,7 +133,7 @@ package GPR2.Build.Tree_Db is
    --  ???
 
    function Propagate_Actions (Self : Object) return Boolean;
-   --  ???
+   --  Call the On_Tree_Propagation subprogram for each new action of the tree
 
    ----------------------------
    -- Iteration on artifacts --
