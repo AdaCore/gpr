e801ee01e7bc46e2fe52119305c66db794c7ef4d
Vincent Jicquel <jicquel@adacore.com>
2024-11-14 10:56:21 +0100

    Use explicitly "gpr2build" for tools/gprbuild tests
    The GPRBUILD variable is set by the testsuite and can contain either
    `gprbuild`, `gpr2build` or a user defined value. For gpr2build tests,
    only `gpr2build` is accepted.

--- testsuite/tests/tools/gprbuild/naming_exception/test.py
+++ testsuite/tests/tools/gprbuild/naming_exception/test.py
@@ -1,15 +1,14 @@
 from testsuite_support.builder_and_runner import BuilderAndRunner
-from testsuite_support.tools import GPRBUILD
 
 bnr = BuilderAndRunner()
 
 def run(cmd):
     print("$ " + " ".join(cmd));
-    if cmd[0] == GPRBUILD:
+    if cmd[0] == "gpr2build":
         bnr.call(cmd)
     else:
         print(bnr.simple_run([cmd], catch_error=True).out)
 
-run([GPRBUILD, "-Pprj", "-p"])
-run([GPRBUILD, "-Pprj", "-p"])
+run(["gpr2build", "-Pprj", "-p"])
+run(["gpr2build", "-Pprj", "-p"])
 run(["./main"])
