c316c6daaabb330d7daf75a24ac63fc74eecc8b5
Jerome Lambourg <lambourg@adacore.com>
2024-11-21 17:32:34 +0100

    Add a test for static C libraries

---
+++ testsuite/tests/tools/gprbuild/libraries/c_lib/src/lib/pkg.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void hello(char* msg) {
+  printf("from the C lib: ");
+  printf(msg);
+  printf("\n");
+}
---
+++ testsuite/tests/tools/gprbuild/libraries/c_lib/test.py
@@ -0,0 +1,54 @@
+import json
+import os
+from e3.fs import mv
+from testsuite_support.builder_and_runner import BuilderAndRunner
+
+bnr = BuilderAndRunner()
+
+def run(cmd):
+    print("$ " + " ".join(cmd));
+    if cmd[0] == "gpr2build":
+        bnr.call(cmd)
+    else:
+        print(bnr.simple_run([cmd], catch_error=True).out)
+
+# Basic check that building demo.gpr produces libdemo.a and that building an
+# exe with it only uses libdemo.a and not any of the objects contained in it.
+
+run(["gpr2build", "-q", "-Pdemo.gpr", "-p", "--json-summary"])
+with open("obj/lib/jobs.json") as fp:
+    cntlib = json.load(fp)
+run(["gpr2build", "-q", "-Papp.gpr", "-p", "--json-summary"])
+with open("obj/app/jobs.json") as fp:
+    cntbin = json.load(fp)
+
+
+print("lib:")
+for job in cntlib:
+    print(
+        "uid: '"
+        + job["uid"]
+        + "', status : '"
+        + job["status"]
+        + "'"
+    )
+
+if os.path.isfile(os.path.join("lib", "libdemo.a")):
+    print("lib has been created, good!")
+else:
+    print("ERROR: cannot find the lib")
+
+print("bin:")
+found = False
+for job in cntbin:
+    if job["status"] != "SKIPPED":
+        if "libdemo.a" in job["command"]:
+            found = True
+            if "pkg.o" in job["command"]:
+                print("ERROR: found both libdemo.a and pkg.o in the link command")
+                print(job["command"])
+if not found:
+    print("ERROR: cannot find libdemo.a in any command issued to build the app")
+else:
+    print("Ok so far")
+run(["./main"])
