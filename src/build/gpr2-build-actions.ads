0e135f51ce7decb2425f2637fc13e9b381e5f004
Jerome Lambourg <lambourg@adacore.com>
2024-12-18 10:27:35 +0100

    Minor: reorder the Build.Actions subprogram declaration
    This groups together the abstract subprograms to be implemented
    by inheriting types.

--- src/build/gpr2-build-actions.ads
+++ src/build/gpr2-build-actions.ads
@@ -73,14 +73,14 @@ package GPR2.Build.Actions is
    --  contain references to its inputs or outputs depending on what is
    --  relevant to make it unique.
 
-   function Valid_Signature (Self : Object) return Boolean;
-   --  Returns whether or not the action is inhibited. This means the loaded
-   --  signature match the current action signature.
-
    function View (Self : Object) return GPR2.Project.View.Object is abstract;
    --  The view that is used for the context of the action's execution. The
    --  view is used to retrieve the switches for the tool, and to know where
    --  the output is stored (the Object_Dir attribute).
+
+   function Working_Directory
+     (Self : Object) return Path_Name.Object is abstract;
+   --  The working directory used in the context of the action's execution
 
    function On_Tree_Insertion
      (Self     : Object;
@@ -88,6 +88,21 @@ package GPR2.Build.Actions is
    --  Function called when Self is added to the tree's database. Allows the
    --  action to add its input and output artifacts and dependencies.
    --  Returns True on success.
+
+   procedure Compute_Command
+     (Self     : in out Object;
+      Slot     : Positive;
+      Cmd_Line : in out GPR2.Build.Command_Line.Object) is abstract;
+   --  Return the command line and environment corresponding to the action
+
+   procedure Compute_Signature
+     (Self      : Object;
+      Signature : in out GPR2.Build.Signature.Object) is abstract;
+   --  This populates the artifacts that are involved in the action.
+
+   function Valid_Signature (Self : Object) return Boolean;
+   --  Returns whether or not the action is inhibited. This means the loaded
+   --  signature match the current action signature.
 
    function On_Tree_Propagation
      (Self : in out Object) return Boolean;
@@ -110,11 +125,6 @@ package GPR2.Build.Actions is
    --  Used to store the signature of the action after it has been executed.
    --  Returns false in case an expected artifact is missing.
 
-   procedure Compute_Signature
-     (Self      : Object;
-      Signature : in out GPR2.Build.Signature.Object) is abstract;
-   --  This populates the artifacts that are involved in the action.
-
    procedure Load_Signature (Self : in out Object);
    --  Compare the current action signature to the loaded signature
 
@@ -132,16 +142,7 @@ package GPR2.Build.Actions is
       Slot : Positive);
    --  Updates the command line and update the signature accordingly
 
-   procedure Compute_Command
-     (Self     : in out Object;
-      Slot     : Positive;
-      Cmd_Line : in out GPR2.Build.Command_Line.Object) is abstract;
-   --  Return the command line and environment corresponding to the action
-
    function Command_Line (Self : Object) return GPR2.Build.Command_Line.Object;
-
-   function Working_Directory
-     (Self : Object) return Path_Name.Object is abstract;
 
    type Execution_Status is (Skipped, Success);
 
