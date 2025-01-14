b5f7a4777044e50e778048cacad6b4a32cf9a357
Vincent Jicquel <jicquel@adacore.com>
2025-01-14 11:24:40 +0100

    Adapt libraries tests to work properly on windows
    Shared libraries are .dll filers on windows instead of .so files on
    linux. This commit makes the test output platform agnostic and
    homogenizes the other libraries tests.

--- testsuite/tests/tools/gprbuild/libraries/shared/import-shared/test.py
+++ testsuite/tests/tools/gprbuild/libraries/shared/import-shared/test.py
@@ -1,10 +1,11 @@
 import json
 import os
-from e3.fs import mv
+import distutils.ccompiler
 from testsuite_support.builder_and_runner import BuilderAndRunner
 
-bnr = BuilderAndRunner()
 
+bnr = BuilderAndRunner()
+shared_lib_ext = distutils.ccompiler.new_compiler().shared_lib_extension
 
 def run(cmd):
     print("$ " + " ".join(cmd))
@@ -14,8 +15,9 @@ def run(cmd):
         print(bnr.simple_run([cmd], catch_error=True).out)
 
 
-# Basic check that building mylib1.gpr produces mylib1.so and that building an
-# exe with it only uses mylib1.so and not any of the objects contained in it.
+# Basic check that building mylib1.gpr produces mylib1.so/dll and that building an
+# exe with it only uses mylib1.so/dll and not any of the objects contained in it.
+
 
 run(["gpr2build", "-q", "-Pmylib1.gpr", "-p", "--json-summary"])
 with open("jobs.json") as fp:
@@ -24,24 +26,20 @@ run(["gpr2build", "-q", "-Papp.gpr", "-p", "--json-summary"])
 with open("jobs.json") as fp:
     cntbin = json.load(fp)
 
-
-print("lib:")
-for job in cntlib:
-    print("uid: '" + job["uid"] + "', status : '" + job["status"] + "'")
-
-if os.path.isfile(os.path.join("lib", "libmylib1.so")):
+if os.path.isfile(os.path.join("lib", "libmylib1" + shared_lib_ext)):
     print("mylib1 has been created, good!")
 else:
-    print("ERROR: cannot find the libmylib1.so")
+    print("ERROR: cannot find the libmylib1" + shared_lib_ext)
 
-if os.path.isfile(os.path.join("lib", "libmylib2.so")):
+if os.path.isfile(os.path.join("lib", "libmylib2" + shared_lib_ext)):
     print("mylib2 has been created, good!")
 else:
-    print("ERROR: cannot find the libmylib2.so")
+    print("ERROR: cannot find the libmylib2" + shared_lib_ext)
 
-print("bin:")
 found = False
 error = False
+
+
 for job in cntbin:
     if job["status"] != "SKIPPED":
         if "-lmylib1" in job["command"] and "-lmylib2" in job["command"]:
@@ -55,9 +53,16 @@ for job in cntbin:
                 "ERROR: The static version of the libs should not be used in the link command"
             )
             error = True
-        if "mylib1.so" in job["command"] or "mylib2.so" in job["command"]:
+        if (
+            "mylib1" + shared_lib_ext in job["command"]
+            or "mylib2" + shared_lib_ext in job["command"]
+        ):
             print(
-                "ERROR: mylib1.so and mylib2.so should not be explicitly added to the command line. Instead, -L and -l option should be used."
+                "ERROR: mylib1"
+                + shared_lib_ext
+                + " and mylib2"
+                + shared_lib_ext
+                + "should not be explicitly added to the command line. Instead, -L and -l option should be used."
             )
             error = True
 
@@ -70,4 +75,9 @@ if not found:
     )
 else:
     print("Ok so far")
+
+# Let windows find the dynamic lib
+os.environ["PATH"] = os.pathsep.join(
+    [os.path.join(os.getcwd(), "lib"), os.environ["PATH"]]
+)
 run(["./main"])
--- testsuite/tests/tools/gprbuild/libraries/shared/simple/test.py
+++ testsuite/tests/tools/gprbuild/libraries/shared/simple/test.py
@@ -1,9 +1,10 @@
 import json
 import os
-from e3.fs import mv
+import distutils.ccompiler
 from testsuite_support.builder_and_runner import BuilderAndRunner
 
 bnr = BuilderAndRunner()
+shared_lib_ext = distutils.ccompiler.new_compiler().shared_lib_extension
 
 def run(cmd):
     print("$ " + " ".join(cmd));
@@ -12,8 +13,8 @@ def run(cmd):
     else:
         print(bnr.simple_run([cmd], catch_error=True).out)
 
-# Basic check that building mylib.gpr produces mylib.so and that building an
-# exe with it only uses mylib.so and not any of the objects contained in it.
+# Basic check that building mylib.gpr produces mylib.so/dll and that building an
+# exe with it only uses mylib.so/dll and not any of the objects contained in it.
 
 run(["gpr2build", "-q", "-Pmylib.gpr", "-p", "--json-summary"])
 with open("jobs.json") as fp:
@@ -23,17 +24,7 @@ with open("jobs.json") as fp:
     cntbin = json.load(fp)
 
 
-print("lib:")
-for job in cntlib:
-    print(
-        "uid: '"
-        + job["uid"]
-        + "', status : '"
-        + job["status"]
-        + "'"
-    )
-
-if os.path.isfile(os.path.join("lib", "libmylib.so")):
+if os.path.isfile(os.path.join("lib", "libmylib" + shared_lib_ext)):
     print("mylib has been created, good!")
 else:
     print("ERROR: cannot find the lib")
@@ -49,11 +40,16 @@ for job in cntbin:
                 print(job["command"])
         if "mylib.a" in job["command"]:
                 print("ERROR: The static version of mylib (mylib.a) should not be used in the link command")
-        if "mylib.so" in job["command"]:
-            print("ERROR: mylib.so should not be explicitly added to the command line. Instead, -L and -l option should be used.");
+        if "mylib" + shared_lib_ext in job["command"]:
+            print("ERROR: mylib" + shared_lib_ext + " should not be explicitly added to the command line. Instead, -L and -l option should be used.");
 
 if not found:
     print("ERROR: cannot find -lmylib in any command issued to build the app")
 else:
     print("Ok so far")
+
+# Let windows find the dynamic lib
+os.environ["PATH"] = os.pathsep.join(
+    [os.path.join(os.getcwd(), "lib"), os.environ["PATH"]]
+)
 run(["./main"])
