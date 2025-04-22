919378cb34daf7d11db3362797cea7226d031129
Jerome Lambourg <lambourg@adacore.com>
2025-04-22 23:32:36 +0200

    Adjust test tools/gprclean/c-switch to use gpr2build
    For eng/gpr/gpr-issues#582

--- testsuite/tests/tools/gprclean/c-switch/test.py
+++ testsuite/tests/tools/gprclean/c-switch/test.py
@@ -1,13 +1,13 @@
 from testsuite_support.builder_and_runner import BuilderAndRunner
-from testsuite_support.tools import GPRCLEAN, GPRBUILD
+from testsuite_support.tools import GPR2CLEAN, GPR2BUILD
 
 
 bnr = BuilderAndRunner()
 
 print('TEST1')
-bnr.check_output([GPRBUILD, '-p', '-q', '-Pfiles/test1', '--subdirs=debug'])
-print(bnr.check_output([GPRCLEAN, '-r', '-n', '-c', '-Pfiles/test1',
+bnr.check_output([GPR2BUILD, '-p', '-q', '-Pfiles/test1', '--subdirs=debug'])
+print(bnr.check_output([GPR2CLEAN, '-r', '-n', '-c', '-Pfiles/test1',
                         '--subdirs=debug']).out)
 print('TEST2')
-bnr.check_output([GPRBUILD, '-p', '-q', '-Pfiles/test2'])
-print(bnr.check_output([GPRCLEAN, '-r', '-n', '-c', '-Pfiles/test2']).out)
+bnr.check_output([GPR2BUILD, '-p', '-q', '-Pfiles/test2'])
+print(bnr.check_output([GPR2CLEAN, '-r', '-n', '-c', '-Pfiles/test2']).out)
