4cca85a0495fd459c12dd1b78ec2bfd8192f619a
Jerome Lambourg <lambourg@adacore.com>
2025-05-20 20:05:50 +0200

    Adjust test for windows

--- testsuite/tests/tools/gprbuild/libraries/standalone/internal-interface-closure/test.py
+++ testsuite/tests/tools/gprbuild/libraries/standalone/internal-interface-closure/test.py
@@ -4,8 +4,9 @@ from testsuite_support.builder_and_runner import BuilderAndRunner
 from testsuite_support.tools import GPR2BUILD
 
 bnr = BuilderAndRunner()
+env = Env()
 
-if 'windows' in Env().target.platform:
+if 'windows' in env.target.platform:
     exeext = '.exe'
 else:
     exeext = ''
@@ -14,13 +15,17 @@ else:
 def run(cmd):
     print("$ " + " ".join(cmd))
     if cmd[0] == GPR2BUILD:
-        bnr.call(cmd)
+        return bnr.call(cmd)
     else:
         print(bnr.simple_run([cmd], catch_error=True).out)
 
-
 # Basic check that building demo.gpr produces libdemo.a and that building an
 # exe with it only uses libdemo.a and not any of the objects contained in it.
+
+# as we produce shared libs, we need to add the path for windows to retrieve
+# them (no rpath mechanism for dlls)
+
+env.add_search_path("PATH", "lib")
 
 for dir in ("library_interface", "interfaces"):
     for app_dir in ("app", "app2"):
