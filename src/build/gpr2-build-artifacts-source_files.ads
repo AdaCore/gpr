4334264b13d6335023c17b632304e5a5bef08771
Vincent Jicquel <jicquel@adacore.com>
2025-09-23 12:23:35 +0200

    Implement a Source_Files artifact type
    This type will be useful to determine if an artifact is a source file.
    
    Issue: eng/gpr/gpr-issues#566

---
+++ src/build/gpr2-build-artifacts-source_files.ads
@@ -0,0 +1,23 @@
+--
+--  Copyright (C) 2024, AdaCore
+--
+--  SPDX-License-Identifier: Apache-2.0 WITH LLVM-Exception
+--
+
+with GPR2.Build.Artifacts.Files;
+with GPR2.Path_Name;
+
+package GPR2.Build.Artifacts.Source_Files is
+
+   type Object is new GPR2.Build.Artifacts.Files.Object with null record;
+
+   overriding function Create
+     (Path : GPR2.Path_Name.Object) return Object is
+      (Files.Create (Path) with null record);
+
+   overriding function Protocol (Self : Object) return String is
+     ("source_file");
+
+   Undefined : constant Object := (Files.Undefined with null record);
+
+end GPR2.Build.Artifacts.Source_Files;
