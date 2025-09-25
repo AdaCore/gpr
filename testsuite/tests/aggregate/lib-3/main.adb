f555254f0d7903bdc1f4638340a924b148d05f54
Jerome Lambourg <lambourg@adacore.com>
2025-09-25 12:30:13 +0200

    Revive aggregate/lib-2 and lib-3 tests

--- testsuite/tests/aggregate/lib-3/main.adb
+++ testsuite/tests/aggregate/lib-3/main.adb
@@ -1,11 +1,14 @@
 with Ada.Strings.Fixed;
 with Ada.Text_IO;
 
-with GPR2.Context;
+with GPR2.Build.Actions.Compile;
+with GPR2.Build.Actions_Population;
+with GPR2.Build.Artifacts.Files;
+with GPR2.Build.Options;
+with GPR2.Build.Source.Sets;
 with GPR2.Path_Name;
+with GPR2.Options;
 with GPR2.Project.Attribute.Set;
-with GPR2.Project.Source.Set;
-with GPR2.Project.Source.Artifact;
 with GPR2.Project.Tree;
 with GPR2.Project.Variable.Set;
 with GPR2.Project.View;
@@ -18,45 +21,62 @@ procedure Main is
 
    procedure Display (Prj : Project.View.Object);
 
-   procedure Output_Filename (Filename : Path_Name.Full_Name);
+   Cwd : constant Path_Name.Object := Path_Name.Create_Directory (".");
 
    -------------
    -- Display --
    -------------
 
    procedure Display (Prj : Project.View.Object) is
-      use GPR2.Project.Attribute.Set;
-      use GPR2.Project.Variable.Set.Set;
+      use type GPR2.Project.View.Object;
+      use GPR2.Build;
    begin
       Text_IO.Put (String (Prj.Name) & " ");
       Text_IO.Set_Col (10);
       Text_IO.Put_Line (Prj.Qualifier'Img);
 
-      for S of Prj.Sources loop
-         Text_IO.Put_Line ("   " & String (S.Path_Name.Base_Name));
+      for A of Prj.Tree.Artifacts_Database.All_Actions loop
+         if A.View = Prj then
+            Text_IO.Put ("   ");
+            if A in Actions.Compile.Object'Class then
+               Text_IO.Put_Line
+                 (String
+                      (Actions.Compile.Object (A).Input.Path_Name.Base_Name));
+            else
+               Text_IO.Put_Line (A.UID.Action_Class);
+            end if;
 
-         for A of S.Artifacts.List loop
-            Text_IO.Put ("      "); Output_Filename (A.Value);
-         end loop;
+            for File of Prj.Tree.Artifacts_Database.Outputs (A.UID) loop
+               if File in Artifacts.Files.Object'Class then
+                  Text_IO.Put_Line
+                    ("      "
+                     & String
+                         (Artifacts.Files.Object (File).Path.Relative_Path
+                            (Cwd)));
+               end if;
+            end loop;
+         end if;
       end loop;
    end Display;
 
-   ---------------------
-   -- Output_Filename --
-   ---------------------
-
-   procedure Output_Filename (Filename : Path_Name.Full_Name) is
-      I : constant Positive :=
-            Strings.Fixed.Index (Filename, "aggregate-lib-3");
-   begin
-      Text_IO.Put_Line (" > " & Filename (I + 16 .. Filename'Last));
-   end Output_Filename;
-
-   Prj : Project.Tree.Object;
-   Ctx : Context.Object;
+   Prj     : Project.Tree.Object;
+   Opt     : Options.Object;
+   DAG_Opt : Build.Options.Build_Options;
 
 begin
-   Project.Tree.Load_Autoconf (Prj, Create ("demo.gpr"), Ctx);
+   Opt.Add_Switch (Options.P, "demo.gpr");
+
+   if not Prj.Load (Opt, Absent_Dir_Error => GPR2.No_Error) then
+      return;
+   end if;
+
+   if not Prj.Update_Sources then
+      return;
+   end if;
+
+   if not Build.Actions_Population.Populate_Actions (Prj, DAG_Opt, True) then
+      return;
+   end if;
 
    for P of Prj loop
       Display (P);
