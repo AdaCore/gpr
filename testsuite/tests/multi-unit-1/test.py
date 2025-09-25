7a88d539923a7b8e1ec4f2184349d5f8c8e7b9bd
Jerome Lambourg <lambourg@adacore.com>
2025-09-25 15:35:46 +0200

    Revive test multi-unit-1

--- testsuite/tests/multi-unit-1/test.py
+++ testsuite/tests/multi-unit-1/test.py
@@ -1,10 +1,17 @@
 import subprocess
 
 from testsuite_support.builder_and_runner import BuilderAndRunner
-from testsuite_support.tools import GPRBUILD
+from testsuite_support.tools import GPRBUILD, GPRCLEAN
 
 bnr = BuilderAndRunner()
 
-bnr.check_output([GPRBUILD, "-p", "-q", "files/multi.gpr"])
 bnr.build("multi.gpr", args=['-p'])
+
+bnr.check_output([GPRBUILD, "-p", "-q", "files/multi.gpr"])
+print("================")
+print("with ALI files:")
+bnr.call(["./main"])
+print("================")
+print("without ALI files:")
+bnr.check_output([GPRCLEAN, "-p", "-q", "files/multi.gpr"])
 bnr.call(["./main"])
