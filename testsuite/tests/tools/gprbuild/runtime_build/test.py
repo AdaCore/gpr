b3831e0b3659f357443182e6b787d6766ba4018d
Jerome Lambourg <lambourg@adacore.com>
2025-10-14 09:45:09 +0200

    Adjust tools/gprbuild/runtime_build on x86-windows
    On x86-windows, as reported by ldd, shared libraries always hold
    a reference to themselves. Adjust the runtime build test accordingly.
    
    For eng/gpr/gpr-issues#617

--- testsuite/tests/tools/gprbuild/runtime_build/test.py
+++ testsuite/tests/tools/gprbuild/runtime_build/test.py
@@ -16,9 +16,9 @@ env = Env()
 def find_rts():
     gnat = which("gcc")
     assert gnat is not None, "cannot find gnat"
-    install_dir = os.path.dirname (os.path.dirname(gnat))
+    install_dir = os.path.dirname(os.path.dirname(gnat))
 
-    lib_dir = glob.glob (os.path.join(install_dir, "lib", "gcc", "*", "*"))
+    lib_dir = glob.glob(os.path.join(install_dir, "lib", "gcc", "*", "*"))
     assert len(lib_dir) == 1, "issue finding the runtime dir"
     lib_dir = lib_dir[0]
 
@@ -26,6 +26,7 @@ def find_rts():
         lib_dir = os.path.join(lib_dir, "rts-native")
 
     return lib_dir
+
 
 # lookup for the native runtime dir
 native_rts = find_rts()
@@ -36,27 +37,38 @@ mkdir(local_rts)
 
 # those must be in an adainclude dir
 adainclude = os.path.join(local_rts, "adainclude")
-sync_tree (os.path.join(native_rts, "adainclude"), adainclude)
+sync_tree(os.path.join(native_rts, "adainclude"), adainclude)
 
 # ??? patch libgnarl.gpr so that it depends on libgnat.gpr. Temporary solution
 # until the project actually depends on libgnat
-with open(os.path.join (adainclude, "libgnarl.gpr")) as fp:
+with open(os.path.join(adainclude, "libgnarl.gpr")) as fp:
     cnt = fp.read().splitlines()
 if 'with "libgnat";' not in cnt:
-    cnt.insert (0, 'with "libgnat";');
-    with open(os.path.join (adainclude, "libgnarl.gpr"), "w") as fp:
-        fp.write('\n'.join(cnt));
+    cnt.insert(0, 'with "libgnat";')
+    with open(os.path.join(adainclude, "libgnarl.gpr"), "w") as fp:
+        fp.write("\n".join(cnt))
 
 # create the library_dir, else gnat will complain (??? it should be created by gprbuild beforehand?)
-mkdir(os.path.join(local_rts,  "adalib"))
+mkdir(os.path.join(local_rts, "adalib"))
 
 # build libgnat then libgnarl
 for lib in "libgnat", "libgnarl":
     print(f"BUILD {lib}...")
-    bnr.check_output([GPRBUILD, f"-P{adainclude}/{lib}.gpr", "-p", "-q", "--json-summary", "--keep-temp-files", "-XLIBRARY_KIND=relocatable", f"--RTS={local_rts}"])
+    bnr.check_output(
+        [
+            GPRBUILD,
+            f"-P{adainclude}/{lib}.gpr",
+            "-p",
+            "-q",
+            "--json-summary",
+            "--keep-temp-files",
+            "-XLIBRARY_KIND=relocatable",
+            f"--RTS={local_rts}",
+        ]
+    )
 
-    print ("check args during link...")
-    summary=os.path.join(adainclude, "jobs.json")
+    print("check args during link...")
+    summary = os.path.join(adainclude, "jobs.json")
 
     with open(summary) as fp:
         cnt = json.load(fp)
@@ -64,11 +76,11 @@ for lib in "libgnat", "libgnarl":
     dep_libs = {}
 
     for action in cnt:
-        if action['uid'].startswith("[Link]") and action['uid'].find(lib) > 0:
+        if action["uid"].startswith("[Link]") and action["uid"].find(lib) > 0:
             # extract the response file used during the link if any
             resp_file = None
 
-            for arg in action['command'].split(' '):
+            for arg in action["command"].split(" "):
                 if arg.startswith("@"):
                     # a response file is used
                     resp_file = arg[1:]
@@ -84,24 +96,32 @@ for lib in "libgnat", "libgnarl":
                     if arg.startswith("-l"):
                         dep_libs[arg] = None
             break
-    print(f"{lib} library deps: " + ' '.join(sorted(dep_libs.keys())))
+    print(f"{lib} library deps: " + " ".join(sorted(dep_libs.keys())))
 
     # check the libs don't depend on external libgnat/libgnarl
 
-    lib_ext = ".dll" if env.host.os.name == "windows" else ".so"
-
-    result = bnr.simple_run(["ldd", os.path.join(local_rts, "adalib", f"{lib}{lib_ext}")]).out
-
     has_libgnat = False
-    for l in result.splitlines():
-        l = l.strip()
-        if l.startswith("libgnarl"):
-            print("error, dependency on libgnat/libgnarl detected")
-            print(l)
-            has_libgnat = True
-        elif l.startswith("libgnat") and lib == "libgnat":
-            print("error, libgnat depends on libgnat")
-            print(l)
-            has_libgnat = True
+
+    # ... except on x86-windows where the dlls always hold a ref to themselves
+    # for some reason
+
+    if env.host.platform != "x86-windows":
+        lib_ext = ".dll" if env.host.os.name == "windows" else ".so"
+
+        result = bnr.simple_run(
+            ["ldd", os.path.join(local_rts, "adalib", f"{lib}{lib_ext}")]
+        ).out
+
+        for l in result.splitlines():
+            l = l.strip()
+            if l.startswith("libgnarl"):
+                print("error, dependency on libgnat/libgnarl detected")
+                print(l)
+                has_libgnat = True
+            elif l.startswith("libgnat") and lib == "libgnat":
+                print("error, libgnat depends on libgnat")
+                print(l)
+                has_libgnat = True
+
     if not has_libgnat:
         print("no dependency on libgnat/libgnarl, good")
