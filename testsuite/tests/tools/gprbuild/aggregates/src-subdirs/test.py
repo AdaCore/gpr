99ff998e4b9136990edc06625b408339e64cc4fb
Pierre BEGUET <pbeguet@adacore.com>
2026-04-14 10:50:35 +0200

    Fix tools__gprbuild__aggregates__src-subdirs failures on x86-windows
    Issue: eng/gpr/gpr-issues#817

--- testsuite/tests/tools/gprbuild/aggregates/src-subdirs/test.py
+++ testsuite/tests/tools/gprbuild/aggregates/src-subdirs/test.py
@@ -2,6 +2,7 @@ from testsuite_support.builder_and_runner import BuilderAndRunner
 from testsuite_support.tools import GPRBUILD, GPRCLEAN
 
 import distutils.ccompiler
+import os
 
 bnr = BuilderAndRunner()
 shared_lib_ext = distutils.ccompiler.new_compiler().shared_lib_extension
@@ -14,7 +15,7 @@ def run(cmd):
         print(bnr.simple_run([cmd], catch_error=True).out)
 
 def check(name, expected):
-    print(f"$ nm {name} | grep \"pkg\"")
+    print(f"$ nm {os.path.splitext(name)[0]} | grep \"pkg\"")
     output = bnr.simple_run(["nm", "-j", name]).out
 
     missing = [s for s in expected if s not in output]
@@ -27,11 +28,11 @@ def check(name, expected):
         else:
             print(f"{', '.join(expected)} symbols were found.")
 
-run([GPRBUILD, "-P", "static.gpr", "--src-subdirs=src2"])
+run([GPRBUILD, "-P", "static.gpr", "--src-subdirs=src2", "-q"])
 check("lib/libstatic.a", ["pkg_E"])
 
-run([GPRBUILD, "-P", "esal.gpr", "--src-subdirs=src2", "-f"])
+run([GPRBUILD, "-P", "esal.gpr", "--src-subdirs=src2", "-f", "-q"])
 check(f"lib/libesal{shared_lib_ext}", ["pkg_E", "pkgS"])
 
-run([GPRBUILD, "-P", "agg_esal.gpr", "--src-subdirs=src2", "-f"])
+run([GPRBUILD, "-P", "agg_esal.gpr", "--src-subdirs=src2", "-f", "-q"])
 check(f"agg_lib/libagg_esal{shared_lib_ext}", ["pkg_E", "pkgS"])
