6388852ea4b576a31bb53ad45b2df3617b341cd8
Pierre BEGUET <pbeguet@adacore.com>
2026-04-14 10:50:35 +0200

    Fix tools__gprbuild__aggregates__src-subdirs failures on x86-windows
    Issue: eng/gpr/gpr-issues#817

--- testsuite/tests/tools/gprbuild/aggregates/src-subdirs/test.py
+++ testsuite/tests/tools/gprbuild/aggregates/src-subdirs/test.py
@@ -13,22 +13,25 @@ def run(cmd):
     else:
         print(bnr.simple_run([cmd], catch_error=True).out)
 
-def check(name):
+def check(name, expected):
     print(f"$ nm {name} | grep \"pkg\"")
     output = bnr.simple_run(["nm", "-j", name]).out
-    symbols=[]
-    for line in output.split("\n"):
-        if "pkg" in line:
-            symbols.append(line)
-    symbols.sort()
-    for symbol in symbols:
-        print(symbol)
+
+    missing = [s for s in expected if s not in output]
+
+    if missing:
+        print(f"Missing symbol : {', '.join(missing)}")
+    else:
+        if len(expected) == 1:
+            print(f"{expected[0]} symbol was found.")
+        else:
+            print(f"{', '.join(expected)} symbols were found.")
 
 run([GPRBUILD, "-P", "static.gpr", "--src-subdirs=src2"])
-check("lib/libstatic.a")
+check("lib/libstatic.a", ["pkg_E"])
 
 run([GPRBUILD, "-P", "esal.gpr", "--src-subdirs=src2", "-f"])
-check(f"lib/libesal{shared_lib_ext}")
+check(f"lib/libesal{shared_lib_ext}", ["pkg_E", "pkgS"])
 
 run([GPRBUILD, "-P", "agg_esal.gpr", "--src-subdirs=src2", "-f"])
-check(f"agg_lib/libagg_esal{shared_lib_ext}")
+check(f"agg_lib/libagg_esal{shared_lib_ext}", ["pkg_E", "pkgS"])
