fba7fab3c2647b7d4298db6e92095d2841b7991e
Eric Botcazou <ebotcazou@adacore.com>
2026-05-13 10:28:35 +0200

    Suppress accessibility checks locally
    When manually fetching the Element discriminant of the result of a call to
    the Reference function.
    
    Issue: eng/toolchain/gnat#1261

--- src/lib/gpr2-project-parser.adb
+++ src/lib/gpr2-project-parser.adb
@@ -3823,6 +3823,10 @@ package body GPR2.Project.Parser is
             P_Name : constant Package_Id :=
                        +Get_Name_Type (Name.As_Single_Tok_Node);
 
+            pragma Suppress (Accessibility_Check);
+            --  The below trick makes it possible to get a reference implicitly
+            --  bypassing tampering checks but it violates accessibility rules.
+
          begin
             --  Entering a package, set the state and parse the corresponding
             --  children.
