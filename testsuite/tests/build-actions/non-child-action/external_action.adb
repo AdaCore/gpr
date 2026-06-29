bc020031a35dbfc33889d447ad11c14ede31bcb0
Vincent Jicquel <jicquel@adacore.com>
2026-06-29 18:26:56 +0200

    Provide a setter for the action signature
    To extend the actions package without creating a child package, users need
    a way to modify the signature. This change introduces a setter for that
    purpose.
    
    Issue: eng/gpr/gpr-issues#853

--- testsuite/tests/build-actions/non-child-action/external_action.adb
+++ testsuite/tests/build-actions/non-child-action/external_action.adb
@@ -4,45 +4,68 @@
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-Exception
 --
 
+with Ada.Exceptions;
+with Ada.Text_IO; use Ada.Text_IO;
+with GPR2.Build.Artifacts.Files;
+with GPR2.Build.Signature;
 with GPR2.Build.Tree_Db;
 with GPR2.Path_Name;
 
 package body External_Action is
 
-   ---------------------
-   -- Compute_Command --
-   ---------------------
-
-   overriding procedure Compute_Command
-     (Self           : in out Object;
-      Slot           : Positive;
-      Cmd_Line       : in out GPR2.Build.Command_Line.Object;
-      Signature_Only : Boolean) is
-   begin
-      --  Does nothing for this test
-      null;
-   end Compute_Command;
+   function Output_File (Self : Object) return GPR2.Build.Artifacts.Files.Object;
+   --  Return the output artifact: <view_name>.txt in the current directory.
 
    -----------------------
    -- Compute_Signature --
    -----------------------
 
    overriding procedure Compute_Signature
-     (Self      : in out Object;
+     (Self            : in out Object;
       Check_Checksums : Boolean)
    is
+      --  External_Action is not a child of GPR2.Build.Actions, so the
+      --  Signature field is private. Use the getter/setter pair to modify
+      --  the signature without direct field access.
+      Sig : GPR2.Build.Signature.Object := Self.Signature;
    begin
-      null;
+      if not Sig.Add_Output (Output_File (Self), Check_Checksums) then
+         return;
+      end if;
+      Self.Set_Signature (Sig);
    end Compute_Signature;
+
+   -------------
+   -- Execute --
+   -------------
+
+   overriding function Execute
+     (Self   : in out Object;
+      Stdout : in out Unbounded_String;
+      Stderr : in out Unbounded_String) return Integer
+   is
+      pragma Unreferenced (Stdout);
+      File_Name : constant String :=
+        String (Output_File (Self).Path.Simple_Name);
+      F         : File_Type;
+   begin
+      Create (F, Out_File, File_Name);
+      Put_Line (F, String (Self.View.Name));
+      Close (F);
+      return 0;
+   exception
+      when E : others =>
+         Append (Stderr, Ada.Exceptions.Exception_Message (E));
+         return 1;
+   end Execute;
 
    ----------------
    -- Initialize --
    ----------------
 
    procedure Initialize
-    (Self       : in out Object;
-     Ctxt       : GPR2.Project.View.Object)
-   is
+     (Self : in out Object;
+      Ctxt : GPR2.Project.View.Object) is
    begin
       Self.Set_View (Ctxt);
    end Initialize;
@@ -55,8 +78,19 @@ package body External_Action is
      (Self : Object;
       Db   : in out GPR2.Build.Tree_Db.Object) return Boolean is
    begin
-      return True;
+      return Db.Add_Output (Self.UID, Output_File (Self));
    end On_Tree_Insertion;
+
+   -----------------
+   -- Output_File --
+   -----------------
+
+   function Output_File (Self : Object) return GPR2.Build.Artifacts.Files.Object is
+   begin
+      return GPR2.Build.Artifacts.Files.Create
+        (GPR2.Path_Name.Create_Directory (".").Compose
+           (GPR2.Filename_Type (String (Self.View.Name) & ".txt")));
+   end Output_File;
 
    ---------
    -- UID --
@@ -67,4 +101,5 @@ package body External_Action is
    begin
       return Result;
    end UID;
+
 end External_Action;
--- testsuite/tests/build-actions/non-child-action/external_action.ads
+++ testsuite/tests/build-actions/non-child-action/external_action.ads
@@ -4,68 +4,70 @@
 --  SPDX-License-Identifier: Apache-2.0 WITH LLVM-Exception
 --
 
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with GPR2.Build.Actions;
-with GPR2.Build.Actions.Process;
-with GPR2.Build.Command_Line;
+with GPR2.Build.Actions.Thread;
 with GPR2.Path_Name;
-with GPR2.Project.Registry.Attribute;
 with GPR2.Project.View;
 with GPR2.Build.Tree_Db;
 
 package External_Action is
 
-   package PRA renames GPR2.Project.Registry.Attribute;
-
    type External_Action_Id is new GPR2.Build.Actions.Action_Id with private;
 
-   type Object is new GPR2.Build.Actions.Process.Object with private;
+   type Object is new GPR2.Build.Actions.Thread.Object with private;
+   --  Thread action that writes the project view name to <view>.txt.
 
    overriding function UID (Self : Object) return GPR2.Build.Actions.Action_Id'Class;
 
    procedure Initialize
-     (Self       : in out Object;
-      Ctxt       : GPR2.Project.View.Object);
+     (Self : in out Object;
+      Ctxt : GPR2.Project.View.Object);
 
    overriding function On_Tree_Insertion
-     (Self     : Object;
-      Db       : in out GPR2.Build.Tree_Db.Object) return Boolean;
+     (Self : Object;
+      Db   : in out GPR2.Build.Tree_Db.Object) return Boolean;
 
    overriding procedure Compute_Signature
-     (Self      : in out Object;
+     (Self            : in out Object;
       Check_Checksums : Boolean);
 
-   overriding procedure Compute_Command
-     (Self           : in out Object;
-      Slot           : Positive;
-      Cmd_Line       : in out GPR2.Build.Command_Line.Object;
-      Signature_Only : Boolean);
+   overriding function Execute
+     (Self   : in out Object;
+      Stdout : in out Unbounded_String;
+      Stderr : in out Unbounded_String) return Integer;
+   --  Create <view>.txt in the current directory containing the view name.
+   --  Returns 0 on success, 1 on I/O failure (error written to Stderr).
 
    overriding function Working_Directory
      (Self : Object) return GPR2.Path_Name.Object;
 
    overriding function Extended (Self : Object) return Object is
-      (raise Constraint_Error with "Unexpected call to Extended");
+     (raise Constraint_Error with "Unexpected call to Extended");
 
 private
 
-   type Object is new GPR2.Build.Actions.Process.Object with null record;
+   type Object is new GPR2.Build.Actions.Thread.Object with null record;
 
    type External_Action_Id is new GPR2.Build.Actions.Action_Id with record
-      Ctxt  : GPR2.Project.View.Object;
+      Ctxt : GPR2.Project.View.Object;
    end record;
 
-   overriding function View (Self : External_Action_Id) return GPR2.Project.View.Object
+   overriding function View
+     (Self : External_Action_Id) return GPR2.Project.View.Object
    is (Self.Ctxt);
 
-   overriding function Action_Class (Self : External_Action_Id) return GPR2.Value_Type is
-     ("External Action");
+   overriding function Action_Class
+     (Self : External_Action_Id) return GPR2.Value_Type
+   is ("External Action");
 
-   overriding function Language (Self : External_Action_Id) return GPR2.Language_Id is
-     (GPR2.No_Language);
+   overriding function Language
+     (Self : External_Action_Id) return GPR2.Language_Id
+   is (GPR2.No_Language);
 
    overriding function Action_Parameter
-     (Self : External_Action_Id) return GPR2.Value_Type is
-     (GPR2.Value_Type (Self.Ctxt.Name));
+     (Self : External_Action_Id) return GPR2.Value_Type
+   is (GPR2.Value_Type (Self.Ctxt.Name));
 
    overriding function Working_Directory
      (Self : Object) return GPR2.Path_Name.Object
